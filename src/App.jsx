598498dda4aab0bccd30e3c6091ad3c4956e4ddb
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-30 08:55:40 -0300

    How to Setup Homepage React Js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Layout/Header'
 import './App.css'
 import Footer from './components/Layout/Footer'
+import Home from './components/Home'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
      <Header></Header>
      <Footer></Footer>
+     <Home></Home>
 
     </>
   )
---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../components/Home.css";
+const Home = () => {
+  return (
+    <div className="clothes-card">
+      <i className="imageContainer">
+        {" "}
+        <img
+          src="https://i.ibb.co/sbsVxrN/pietra-schwarzler-l-SLq-x-Qd-FNI-unsplash.jpg"
+          alt=""
+          className="card-image"
+        />
+      </i>
+      <div className="card-details">
+        <h3 className="card-title"></h3>
+        <p className="card-price">Preço:R$56.99</p>
+        <button className="btn-saiba-mais">Saiba Mais</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
