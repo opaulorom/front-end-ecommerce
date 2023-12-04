47c4ae39dd5360cddb43fc858d08e5464f9f953b
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-12-04 13:43:45 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/search/*" element={<Home />} />
+          <Route path="/search/*" element={<Home key="search" />} />
           <Route path="/product/:id" element={<ProductDetails />} />
         </Routes>
       </Router>
