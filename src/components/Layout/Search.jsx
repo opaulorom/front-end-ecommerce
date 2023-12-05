e142c1455d5110fd51b795ff33db1f69f94c9008
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-12-05 12:53:07 -0300

    ajuste

--- src/components/Layout/Search.jsx
+++ src/components/Layout/Search.jsx
@@ -3,11 +3,12 @@ import { useNavigate } from "react-router-dom";
 
 const Search = () => {
   const [keyword, setKeyword] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
   const navigate = useNavigate();
 
   const searchHandler = (e) => {
     e.preventDefault();
+    console.log(keyword, "keyword")
+
     if (keyword.trim()) {
       navigate(`/search?keyword=${encodeURIComponent(keyword)}`);
     } else {
@@ -15,25 +16,13 @@ const Search = () => {
     }
   };
 
-  const fetchSuggestions = async () => {
-    // Implemente a lógica para buscar sugestões com base no keyword
-    // Isso pode ser uma chamada à API ou uma lógica local
-    // Aqui está um exemplo simples
-    const response = await fetch(`http://localhost:3001/api/products/suggestions?keyword=${keyword}`);
-    const data = await response.json();
-    setSuggestions(data.suggestions);
-  };
-
   useEffect(() => {
     if (keyword.trim()) {
-      // Chame a função para buscar sugestões quando o usuário digitar
-      fetchSuggestions();
-    } else {
-      // Limpe as sugestões se o campo de pesquisa estiver vazio
-      setSuggestions([]);
+      // Realize a ação de pesquisa aqui, por exemplo, redirecione para a página de resultados
+      navigate(`/search?keyword=${encodeURIComponent(keyword)}`);
     }
-  }, [keyword]);
-
+  }, [keyword, navigate]);
+  
   return (
     <form onSubmit={searchHandler}>
       <input
@@ -43,13 +32,6 @@ const Search = () => {
         onChange={(e) => setKeyword(e.target.value)}
       />
       <button type="submit">Pesquisar</button>
-
-      {/* Renderize as sugestões */}
-      <ul>
-        {suggestions.map((suggestion, index) => (
-          <li key={index}>{suggestion}</li>
-        ))}
-      </ul>
     </form>
   );
 };
