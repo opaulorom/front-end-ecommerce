5fe0c475a360979163f5d170dfd9184146a9d183
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-12-05 16:38:48 -0300

    ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,20 @@ import Header from "./components/Layout/Header";
 import Footer from "./components/Layout/Footer";
 import Home from "./components/Home";
 import ProductDetails from "./components/product/ProductDetails";
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';  // Importe do pacote 'devtools'
-
-// Crie um novo QueryClient
-const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
-      <>
-        <Header />
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search/*" element={<Home key="search" />} />
-            <Route path="/product/:id" element={<ProductDetails />} />
-          </Routes>
-        </Router>
-        <Footer />
-      </>
-    </QueryClientProvider>
+    <>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search/*" element={<Home key="search" />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </>
   );
 }
 
