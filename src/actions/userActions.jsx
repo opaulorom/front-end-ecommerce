19b94f18450a045b0d4d30167aef0a407f5fd46d
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-12-06 18:24:17 -0300

    ajuste

--- src/actions/userActions.jsx
+++ src/actions/userActions.jsx
@@ -40,30 +40,23 @@ export const login = (email, password) => async (dispatch) => {
 };
 
 // Register user
-
+// Register user
 export const register = (userData) => async (dispatch) => {
     try {
       dispatch({
         type: REGISTER_USER_REQUEST,
       });
   
-      // C/reate FormData object
-      const formData = new FormData();
-  
-      // Append only name, email, and password to FormData
-      formData.append("name", userData.name);
-      formData.append("email", userData.email);
-      formData.append("password", userData.password);
-  
       const config = {
         headers: {
-          "Content-Type": "multipart/form-data",
+          "Content-type": "application/json",
         },
       };
   
       const { data } = await axios.post(
         "http://localhost:3001/api/register",
-        formData,
+        // Use userData.name, userData.email, userData.password here
+        { name: userData.name, email: userData.email, password: userData.password },
         config
       );
   
@@ -82,7 +75,6 @@ export const register = (userData) => async (dispatch) => {
       });
     }
   };
-  
   
 // limpar errors
 export const cleanErrors = () => async (dispatch) => {
