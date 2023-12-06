ccd29fce7f694c6e1a59f30fcca0076b18acb57a
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-12-06 12:12:08 -0300

    How to register user and save in database

--- src/actions/userActions.jsx
+++ src/actions/userActions.jsx
@@ -1,40 +1,77 @@
-import axios from "axios"
+import axios from "axios";
 import {
-    LOGIN_REQUEST,
-    LOGIN_SUCCESS,
-    LOGIN_FAIL,
-    CLEAR_ERRORS
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  CLEAR_ERRORS,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
 } from "../constants/userContants";
 
 // login
 export const login = (email, password) => async (dispatch) => {
-    try{
-        dispatch({
-            type:LOGIN_REQUEST
-        })
+  try {
+    dispatch({
+      type: LOGIN_REQUEST,
+    });
 
-        const config = {
-            headers:{
-                "Content-type":"application/json"
-            }
-        }
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
 
-        const { data} = await axios.post("http://localhost:3001/api/login",{ email, password}, config)
-        dispatch({
-            type:LOGIN_SUCCESS,
-            payload:data.user
-        })
+    const { data } = await axios.post(
+      "http://localhost:3001/api/login",
+      { email, password },
+      config
+    );
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGIN_FAIL,
+      payload: error.response ? error.response.data.message : "Unknown error",
+    });
+  }
+};
+
+// Register user
+export const register = (userData) => async (dispatch) => {
+    try {
+      dispatch({
+        type: REGISTER_USER_REQUEST,
+      });
+  
+      const config = {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      };
+  
+      const { data } = await axios.post(
+        "http://localhost:3001/api/login",
+        userData,
+        config
+      );
+      dispatch({
+        type: REGISTER_USER_SUCCESS,
+        payload: data.user,
+      });
     } catch (error) {
-        dispatch({
-            type:LOGIN_FAIL,
-            payload: error.response ? error.response.data.message : "Unknown error"
-        })
+      dispatch({
+        type: REGISTER_USER_FAIL,
+        payload: error.response ? error.response.data.message : "Unknown error",
+      });
     }
-}
-
+  };
+  
 // limpar errors
 export const cleanErrors = () => async (dispatch) => {
-    dispatch({
-        type: CLEAR_ERRORS
-    });
-  };
+  dispatch({
+    type: CLEAR_ERRORS,
+  });
+};
--- src/reducers/userReducers.jsx
+++ src/reducers/userReducers.jsx
@@ -1,40 +1,45 @@
 import {
-    LOGIN_REQUEST,
-    LOGIN_SUCCESS,
-    LOGIN_FAIL,
-    CLEAR_ERRORS
-} from "../constants/userContants"
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  CLEAR_ERRORS,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
+} from "../constants/userContants";
 
-export const authReducer = (state = {user: {} },action) => {
-    switch(action.type){
-        case LOGIN_REQUEST:
-            return {
-                loading:true,
-                isAuthenticated:false,
-            }
-            case LOGIN_SUCCESS:
-                return {
-                    ...state,
-                    loading:false,
-                    isAuthenticated:true,
-                    user:action.payload
-                }
-            case LOGIN_FAIL:
-                return{
-                    ...state,
-                    isAuthenticated:false,
-                    user:null,
-                    error:action.payload
-                }
+export const authReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case LOGIN_REQUEST:
+    case REGISTER_USER_REQUEST:
+      return {
+        loading: true,
+        isAuthenticated: false,
+      };
+    case LOGIN_SUCCESS:
+    case REGISTER_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: true,
+        user: action.payload,
+      };
+    case LOGIN_FAIL:
+    case REGISTER_USER_FAIL:
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+        error: action.payload,
+      };
 
-            case CLEAR_ERRORS:
-                return {
-                    ...state,
-                    error:null
-                }
-               
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
 
-        default:
-            return state
-    }
-}
+    default:
+      return state;
+  }
+};
