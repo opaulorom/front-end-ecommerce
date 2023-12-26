797bedd0bc08887c9d177c80b33a7a3da9096fcc
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-12-26 10:45:22 -0300

    add

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -6,34 +6,38 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    const [activeLink, setActiveLink] = useState(null)
-    const handleItemClick = (index) => {
-        setActiveLink(index);
-        // Aqui você pode adicionar a lógica para lidar com o clique no link, se necessário
-      };
+  const [activeLink, setActiveLink] = useState(null);
+
+  const handleItemClick = (index) => {
+    setActiveLink(index);
+  };
+
+  const isActive = (index) => {
+    return index === activeLink ? "active" : "";
+  };
+
   return (
     <div className="navbar">
-      <Link to={"/home"}>
-        <div className="nav-item">
+      <Link to={"/home"} >
+        <div onClick={() => handleItemClick(0)} className={`nav-item ${isActive(0)}`}>
           <FaHome />
           <span>Home</span>
         </div>
       </Link>
       <Link to={"/categorias"}>
-        <div className="nav-item">
+        <div  onClick={() => handleItemClick(1)} className={`nav-item ${isActive(1)}`}>
           <FaList />
           <span>Categorias</span>
         </div>
       </Link>
-      <Link to={"/carrinho"}>
-        <div className="nav-item">
-          <FaShoppingCart />
+      <Link to={"/carrinho"} >
+        <div onClick={() => handleItemClick(2)} className={`nav-item ${isActive(2)}`}>
+          <FaShoppingCart/>
           <span>Carrinho</span>
         </div>
       </Link>
-
-      <Link to={"/perfil"}>
-        <div className="nav-item">
+      <Link to={"/perfil"} >
+        <div  onClick={() => handleItemClick(3)} className={`nav-item ${isActive(3)}`}>
           <FaUser />
           <span>Perfil</span>
         </div>
