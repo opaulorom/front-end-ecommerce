e285449e36abf283541bf8ecbf65a633205b6ec3
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-11 20:09:08 -0300

    add

--- front-end/src/components/SearchInput.jsx
+++ front-end/src/components/SearchInput.jsx
@@ -1,62 +1,86 @@
-import React, { useState, useEffect } from "react";
-import "./SearchInput.css"; // Certifique-se de ter um arquivo CSS associado
+import React, { useState, useEffect, useRef } from "react";
+import "./SearchInput.css";
 
 const SearchInput = ({ placeholder, onSearch }) => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [isMobile, setIsMobile] = useState(window.innerWidth >= 700);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 699);
+  const [isMobileInputOpen, setIsMobileInputOpen] = useState(false);
+  const searchContainerRef = useRef();
 
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 699);
     };
 
-    // Adiciona um ouvinte de redimensionamento no montar do componente
     handleResize();
-
-    // Adiciona um ouvinte de redimensionamento
     window.addEventListener("resize", handleResize);
 
-    // Remove o ouvinte de redimensionamento quando o componente for desmontado
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        searchContainerRef.current &&
+        !searchContainerRef.current.contains(event.target)
+      ) {
+        setIsMobileInputOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   const handleSearch = () => {
-    // Lógica de pesquisa aqui (pode chamar a função onSearch)
-    // Exemplo: onSearch(inputValue);
+    setIsMobileInputOpen((prev) => !prev);
+  };
+
+  const closeInput = () => {
+    setIsMobileInputOpen(false);
   };
 
   return (
     <>
       {isMobile ? (
-        <div className="search-container">
-       
+        <div className="search-container" ref={searchContainerRef}>
           <img
             src="https://i.ibb.co/By6TFwq/search-interface-symbol-1.png"
             alt="Search"
             className="search-icon"
             onClick={handleSearch}
           />
+          {isMobileInputOpen && (
+            <div onClick={closeInput}>
+              <input
+                type="text"
+                placeholder={placeholder}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+            </div>
+          )}
         </div>
       ) : (
         <div className="search-container">
-        <input
-          type="text"
-          placeholder={placeholder}
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <img
-          src="https://i.ibb.co/RQNdWdY/search-interface-symbol-5.png"
-          alt="Search"
-          className="search-icon"
-          onClick={handleSearch}
-        />
-  
-
-    
-      </div>
+          <input
+            type="text"
+            placeholder={placeholder}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <img
+            src="https://i.ibb.co/RQNdWdY/search-interface-symbol-5.png"
+            alt="Search"
+            className="search-icon"
+            onClick={handleSearch}
+          />
+        </div>
       )}
     </>
   );
