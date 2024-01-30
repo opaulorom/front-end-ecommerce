17465034889c5b6a15b5ea41f101b9cf753c7d5b
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-30 17:35:59 -0300

    add

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,25 +1,34 @@
 // SearchResults.jsx
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 const SearchResults = () => {
   const { query } = useParams();
   const [searchResults, setSearchResults] = useState([]);
+  const [totalProducts, setTotalProducts] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const fetchSearchResults = async () => {
       try {
-        const response = await fetch(`http://localhost:3001/api/search/product?searchQuery=${query}`);
+        const response = await fetch(`http://localhost:3001/api/search/product?searchQuery=${query}&page=${currentPage}`);
         const data = await response.json();
         console.log('Data from server:', data);
-        setSearchResults(data); // Atualize para setSearchResults(data) se a propriedade correta for 'data'
+        setSearchResults(data.products);
+        setTotalProducts(data.totalProducts);
       } catch (error) {
         console.error('Erro ao buscar resultados de pesquisa:', error);
       }
     };
 
     fetchSearchResults();
-  }, [query]);
+  }, [query, currentPage]);
+
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+  };
 
   return (
     <div>
@@ -31,6 +40,17 @@ const SearchResults = () => {
           </li>
         ))}
       </ul>
+
+      <Stack spacing={2}>
+        <Pagination
+          count={Math.ceil(totalProducts / 10)}  // Defina o número total de páginas com base no total de produtos e pageSize
+          variant="outlined"
+          color="primary"
+          size="large"
+          page={currentPage}
+          onChange={handlePageChange}
+        />
+      </Stack>
     </div>
   );
 };
