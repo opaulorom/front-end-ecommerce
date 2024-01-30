fde9c9ac068ed6b1c2aaa5d0c69dc903f61d76a8
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-30 18:52:57 -0300

    add

---
+++ src/components/CategoriesList.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const CategoriesList = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/api/allCategories');
+        const data = await response.json();
+
+        setCategories(prevCategories => {
+          // Use um conjunto temporário para armazenar categorias únicas
+          const uniqueCategoriesSet = new Set([...prevCategories.map(c => c.category), ...data.map(c => c.category)]);
+          const uniqueCategories = Array.from(uniqueCategoriesSet).map(category => ({ category }));
+
+          return uniqueCategories;
+        });
+      } catch (error) {
+        console.error('Erro ao obter categorias:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div style={{ marginTop: '15rem' }}>
+    <ul style={{ listStyleType: 'none', padding: 0, display: 'flex', flexDirection: 'row', gap: '1rem' }}>
+      {categories.map((category) => (
+        <li key={category.category}>
+          <Link to={`/categories/${category.category}`} style={{ textDecoration: 'none' }}>
+            {category.category}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+  );
+};
+
+export default CategoriesList;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import "./Header.css";
-import SearchInput from "./SearchInput";
-import { useAtom } from 'jotai';
-import { searchAtom } from "../Jotai/searchAtom";
+
 import SearchBar from "./SearchBar";
+import CategoriesList from "./CategoriesList";
 
 const Header = () => {
-  const [searchTerm, setSearchTerm] = useAtom(searchAtom);
-
-  const handleSearch = (term) => {
-    setSearchTerm(term);
-  };
+  
   return (
+    <>
     <div className="ContainerHeader">
       <h1>Logo</h1>
       <div style={{marginRight:"5rem"}}>
         {" "}
         <SearchBar/>
       </div>
-    </div>
+    
+    </div>  <div>
+      <CategoriesList/>
+
+      </div>
+    </>
   );
 };
 
