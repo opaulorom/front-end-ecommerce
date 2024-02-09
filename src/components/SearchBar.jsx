d49e0bccc55df1f905552d01e23107f1b20a7528
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-09 10:32:15 -0300

    add

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,27 +1,33 @@
-// SearchBar.jsx
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-// SearchBar.jsx
+
 const SearchBar = () => {
-    const [searchQuery, setSearchQuery] = useState('');
-    const navigate = useNavigate();
-  
-    const handleSearch = () => {
-      console.log('Search Query:', searchQuery);
-      navigate(`/search/product/${searchQuery}`);
-    };
-  
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="Digite o nome do produto"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button onClick={handleSearch}>Pesquisar</button>
-      </div>
-    );
+  const [searchQuery, setSearchQuery] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    console.log('Search Query:', searchQuery);
+    navigate(`/search/product/${searchQuery}`);
   };
-  
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Digite o nome do produto"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        onKeyPress={handleKeyPress} // Adiciona o event listener para capturar a tecla pressionada
+      />
+      <button onClick={handleSearch}>Pesquisar</button>
+    </div>
+  );
+};
+
 export default SearchBar;
