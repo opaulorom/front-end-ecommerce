aa7be786c3fe2c256e724c340486788658423bb3
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-11 13:25:31 -0300

    add

---
+++ src/components/CategoriesMobile.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import ImageGallery from './ImageGallery';
+import styles from "./Categories.module.css"
+
+const CategoriesMobile = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://serveradmin-whhj.onrender.com/api/allCategories');
+        const data = await response.json();
+
+        setCategories(prevCategories => {
+          // Use um conjunto temporário para armazenar categorias únicas
+          const uniqueCategoriesSet = new Set([...prevCategories.map(c => c.category), ...data.map(c => c.category)]);
+          const uniqueCategories = Array.from(uniqueCategoriesSet).map(category => ({ category }));
+
+          return uniqueCategories;
+        });
+      } catch (error) {
+        console.error('Erro ao obter categorias:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div style={{
+      marginTop:"15rem",
+      marginBottom:"10rem"
+
+    }} className={styles.ImageGallery}>
+            <ImageGallery />
+    
+
+    </div>
+  );
+};
+
+export default CategoriesMobile;
