a824be7b3a9cc15a90c27acb8b9f236ae3eff1e6
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-14 11:59:28 -0300

    add

--- src/components/DiscountImageLink.jsx
+++ src/components/DiscountImageLink.jsx
@@ -2,11 +2,30 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
 
-const DiscountImageLink = ({ imageUrl, alt }) => (
-  <Link to={`/productsByDiscountPercentage/70`}>
-    <img src={imageUrl} alt={alt} />
-    fdfdf
-  </Link>
-);
+const DiscountImageLink = ({ alt }) => {
+  const [imageUrl, setImageUrl] = useState('');
+
+  // Fetch image URL from API on component mount
+  useEffect(() => {
+    const fetchImageUrl = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/api/bannerByDiscount/50');
+        setImageUrl(response.data.banners[0].image); // Assuming you want the first banner
+      } catch (error) {
+        console.error('Error fetching image URL:', error);
+        // Handle error gracefully, e.g., display a placeholder image or error message
+      }
+    };
+
+    fetchImageUrl();
+  }, []);
+
+  return (
+    <Link to={`/productsByDiscountPercentage/70`}>
+      <img src={imageUrl} alt={alt} />
+      fdfdf
+    </Link>
+  );
+};
 
 export default DiscountImageLink;
