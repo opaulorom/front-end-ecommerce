7fdf6949df29ae80984740def5d204e80960813b
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-16 12:50:19 -0300

    add

--- src/components/CategorySubcategories.jsx
+++ src/components/CategorySubcategories.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import CustomPagination from './CustomPagination';
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import CustomPagination from "./CustomPagination";
 
 const CategorySubcategories = () => {
   const { category } = useParams();
@@ -19,7 +19,7 @@ const CategorySubcategories = () => {
     try {
       const queryString = Object.entries(filters)
         .map(([key, value]) => `${key}=${value}`)
-        .join('&');
+        .join("&");
 
       const response = await fetch(
         `http://localhost:3001/api/categories/${category}/mixedProducts?page=${page}&${queryString}`
@@ -28,27 +28,35 @@ const CategorySubcategories = () => {
       setMixedProducts(data.mixedProducts);
       setTotalPages(data.totalPages);
     } catch (error) {
-      console.error('Erro ao obter produtos misturados:', error);
+      console.error("Erro ao obter produtos misturados:", error);
     }
   };
 
   useEffect(() => {
     const fetchSubcategories = async () => {
       try {
-        const response = await fetch(`http://localhost:3001/api/subcategories/${category}`);
+        const response = await fetch(
+          `http://localhost:3001/api/subcategories/${category}`
+        );
         const data = await response.json();
         setSubcategories(data);
       } catch (error) {
-        console.error('Erro ao obter subcategorias:', error);
+        console.error("Erro ao obter subcategorias:", error);
       }
     };
 
     const fetchFilters = async () => {
       try {
         // Alterações nas chamadas de API para obter cores, tamanhos e faixas de preço específicos
-        const colorsResponse = await fetch(`https://serveradmin-whhj.onrender.com/api/categories/${category}/colors`);
-        const sizesResponse = await fetch(`https://serveradmin-whhj.onrender.com/api/categories/${category}/sizes`);
-        const priceRangesResponse = await fetch(`https://serveradmin-whhj.onrender.com/api/categories/${category}/priceRanges`);
+        const colorsResponse = await fetch(
+          `https://serveradmin-whhj.onrender.com/api/categories/${category}/colors`
+        );
+        const sizesResponse = await fetch(
+          `https://serveradmin-whhj.onrender.com/api/categories/${category}/sizes`
+        );
+        const priceRangesResponse = await fetch(
+          `https://serveradmin-whhj.onrender.com/api/categories/${category}/priceRanges`
+        );
 
         const colorsData = await colorsResponse.json();
         const sizesData = await sizesResponse.json();
@@ -60,23 +68,25 @@ const CategorySubcategories = () => {
 
         // Atualizar o conjunto único de tamanhos
         const allSizes = sizesData.reduce((acc, size) => {
-          size.split(',').forEach(s => acc.add(s.trim()));
+          size.split(",").forEach((s) => acc.add(s.trim()));
           return acc;
         }, new Set());
         setUniqueSizes(allSizes);
       } catch (error) {
-        console.error('Erro ao obter opções de filtros:', error);
+        console.error("Erro ao obter opções de filtros:", error);
       }
     };
 
     const fetchOriginalProducts = async () => {
       try {
         // Alteração na chamada de API para obter produtos misturados específicos
-        const response = await fetch(`https://serveradmin-whhj.onrender.com/api/categories/${category}/mixedProducts`);
+        const response = await fetch(
+          `https://serveradmin-whhj.onrender.com/api/categories/${category}/mixedProducts`
+        );
         const data = await response.json();
         setOriginalProducts(data.mixedProducts);
       } catch (error) {
-        console.error('Erro ao obter produtos misturados:', error);
+        console.error("Erro ao obter produtos misturados:", error);
       }
     };
 
@@ -94,96 +104,124 @@ const CategorySubcategories = () => {
     const filters = {
       [filterType]: value,
     };
-  
+
     // Se o filtro for cor ou tamanho, encontrar todos os produtos com a mesma cor ou tamanho
-    if (filterType === 'color' || filterType === 'size') {
+    if (filterType === "color" || filterType === "size") {
       let filteredProducts = originalProducts;
-  
-      if (filterType === 'size') {
-        filteredProducts = originalProducts.filter(product => {
-          const productSizes = (product.size || '').split(',').map(size => size.trim());
-          return productSizes.includes(value) || productSizes.some(size => value.includes(size));
+
+      if (filterType === "size") {
+        filteredProducts = originalProducts.filter((product) => {
+          const productSizes = (product.size || "")
+            .split(",")
+            .map((size) => size.trim());
+          return (
+            productSizes.includes(value) ||
+            productSizes.some((size) => value.includes(size))
+          );
         });
-      } else if (filterType === 'color') {
-        filteredProducts = originalProducts.filter(product => {
-          const productColors = product.variations.map(variation => (variation.color || '').trim());
+      } else if (filterType === "color") {
+        filteredProducts = originalProducts.filter((product) => {
+          const productColors = product.variations.map((variation) =>
+            (variation.color || "").trim()
+          );
           return productColors.includes(value);
         });
       }
-  
+
       setMixedProducts(filteredProducts);
       setTotalPages(1); // Reiniciar a página para a primeira ao aplicar um novo filtro
     }
-  
+
     fetchMixedProducts(1, filters);
   };
-  
-  
 
   return (
-    <div style={{
-      display:"flex"
-    }}>
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
       <div>
-      <h1>Subcategories of {category}</h1>
-      <ul>
-        {subcategories.map((subcategory, index) => (
-          <li key={index}>
-            <Link to={`/categories/${category}/${subcategory}`}>
-              {subcategory}
-            </Link>
-          </li>
-        ))}
-      </ul>
+        <h1>Subcategories of {category}</h1>
+        <ul>
+          {subcategories.map((subcategory, index) => (
+            <li key={index}>
+              <Link to={`/categories/${category}/${subcategory}`}>
+                {subcategory}
+              </Link>
+            </li>
+          ))}
+        </ul>
 
-      <h2>Filtros:</h2>
+        <h2>Filtros:</h2>
+        <div>
+          <h3>Cores</h3>
+          {colors.map((color, index) => (
+            <div key={index} onClick={() => handleFilterClick("color", color)}>
+              {color}
+            </div>
+          ))}
+        </div>
+
+        <div>
+          <h3>Tamanhos</h3>
+          {Array.from(uniqueSizes).map((size, index) => (
+            <div key={index} onClick={() => handleFilterClick("size", size)}>
+              {size}
+            </div>
+          ))}
+        </div>
+
+        <div>
+          <h3>Faixas de Preço</h3>
+          {priceRanges.map((range, index) => (
+            <div
+              key={index}
+              onClick={() => handleFilterClick("priceRange", range)}
+            >
+              {range}
+            </div>
+          ))}
+        </div>
+      </div>
       <div>
-        <h3>Cores</h3>
-        {colors.map((color, index) => (
-          <div key={index} onClick={() => handleFilterClick('color', color)}>
-            {color}
-          </div>
-        ))}
-      </div>
+        <h2>Mixed Products of {category}</h2>
+        <ul
+          style={{
+            listStyleType: "none",
+            padding: 0,
+            margin: 0,
+            display: "grid",
+            gridTemplateColumns: "repeat(4, 1fr)",
+            gap: "20px",
+          }}
+        >
+          {mixedProducts &&
+            mixedProducts.map((product) => (
+              <li key={product._id || "undefined"}>
+                <Link to={`/products/${product._id}`}>
+                  <img
+                    src={product.variations[0].urls[0]}
+                    alt={product.name}
+                    style={{ width: "100%", marginBottom: "10px" }}
+                  />
+                  <div style={{ display: "flex", flexDirection: "column" }}>
+                    <span>{product.name}</span>
+                    <span>
+                      {Number(product.price).toFixed(2).padStart(5, "0")}
+                    </span>
+                  </div>
+                </Link>
+              </li>
+            ))}
+        </ul>
 
-      <div>
-        <h3>Tamanhos</h3>
-        {Array.from(uniqueSizes).map((size, index) => (
-          <div key={index} onClick={() => handleFilterClick('size', size)}>
-            {size}
-          </div>
-        ))}
+        <CustomPagination
+          totalPages={totalPages}
+          currentPage={currentPage}
+          onChangePage={handlePageChange}
+        />
       </div>
-
-      <div>
-        <h3>Faixas de Preço</h3>
-        {priceRanges.map((range, index) => (
-          <div key={index} onClick={() => handleFilterClick('priceRange', range)}>
-            {range}
-          </div>
-        ))}
-      </div>
-      </div>
-      <div>
-      <h2>Mixed Products of {category}</h2>
-<ul style={{ listStyleType: 'none', padding: 0, margin: 0, display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gap: "20px" }}>
-  {mixedProducts && mixedProducts.map(product => (
-    <li key={product._id || 'undefined'}>
-          <Link to={`/products/${product._id}`}>
-      <img src={product.variations[0].urls[0]} alt={product.name} style={{ width: "100%", marginBottom: "10px" }} />
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <span>{product.name}</span>
-        <span>{product.price}</span>
-      </div>
-      </Link>
-    </li>
-  ))}
-</ul>
-
-<CustomPagination totalPages={totalPages} currentPage={currentPage} onChangePage={handlePageChange} />
-
-</div>
-
     </div>
   );
 };
--- src/components/Subcategory.jsx
+++ src/components/Subcategory.jsx
@@ -1,6 +1,6 @@
 // Subcategory.js
-import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 
 const Subcategory = () => {
   const { category, subcategory } = useParams();
@@ -10,12 +10,14 @@ const Subcategory = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch(`http://localhost:3001/api/subcategoriesAndProducts/${category}/${subcategory}`);
+        const response = await fetch(
+          `http://localhost:3001/api/subcategoriesAndProducts/${category}/${subcategory}`
+        );
         const data = await response.json();
         setProducts(data);
         setLoading(false);
       } catch (error) {
-        console.error('Erro ao obter produtos da subcategoria:', error);
+        console.error("Erro ao obter produtos da subcategoria:", error);
         setLoading(false);
       }
     };
@@ -30,19 +32,29 @@ const Subcategory = () => {
   return (
     <div>
       <h1>{subcategory} Products</h1>
-      <ul style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", }}>
-        {products.map(product => (
+      <ul
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          listStyleType: "none",
+        }}
+      >
+        {products.map((product) => (
           <Link to={`/products/${product._id}`}>
-          <li key={product._id}>
-          <img src={product.variations[0].urls[0]} alt={product.name} style={{width: "20svw",
-          }}/>
-            {product.name} - {product.price}
-          </li>
-        </Link>
-        
+            <li key={product._id}>
+              <img
+                src={product.variations[0].urls[0]}
+                alt={product.name}
+                style={{ width: "20svw" }}
+              />
+               <div style={{ display: "flex", flexDirection: "column" }}>
+              <span>{product.name}</span>
+              <span>{Number(product.price).toFixed(2).padStart(5, "0")}</span>
+              </div>
+            </li>
+          </Link>
         ))}
       </ul>
-      
     </div>
   );
 };
