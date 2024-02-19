9b17b5283be5e0393becfeb671d5f25953f7a9ce
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-19 11:06:48 -0300

    add

--- src/components/Heart.jsx
+++ src/components/Heart.jsx
@@ -1,29 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import { useClerk } from "@clerk/clerk-react";
+import React from 'react'
 
-const Heart = ({ productId, clerkUserId }) => {
-  const [isFavorite, setIsFavorite] = useState(false);
-  const clerk = useClerk();
-  useEffect(() => {
-    // Aqui você pode fazer uma requisição para o backend para verificar se o produto está nos favoritos do usuário
-    // Você pode usar o productId e clerkUserId para fazer a requisição
-    // Por exemplo, você pode usar o fetch() ou axios para fazer a requisição
-    fetch(`http://localhost:3001/api/favorites/${clerk.userId}`)
-      .then(response => response.json())
-      .then(data => {
-        // Verificar se o produto está nos favoritos do usuário
-        const isProductInFavorites = data.favorites.some(favorite => favorite._id === productId);
-        setIsFavorite(isProductInFavorites);
-      })
-      .catch(error => console.error('Erro ao verificar se o produto está nos favoritos:', error));
-  }, [productId, clerk.userId]);
-
+const Heart = () => {
   return (
-    <div>
-      {isFavorite ? <FavoriteBorderIcon color="red" /> : <FavoriteBorderIcon color="red" />}
-    </div>
-  );
-};
+    <div>Heart</div>
+  )
+}
 
-export default Heart;
+export default Heart
