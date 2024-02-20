af748bf92721a90c5536d2d6fcd55f2a4b874a03
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-20 12:24:58 -0300

    add

--- src/components/IconToggle.jsx
+++ src/components/IconToggle.jsx
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useUser } from "@clerk/clerk-react";
+import Heart from "react-heart"
 
 const IconToggle = ({ productId }) => {
   const [isFavorite, setIsFavorite] = useState(false);
   const { isSignedIn, user, isLoaded } = useUser();
+  const [active, setActive] = useState(localStorage.getItem('active') === 'true' ? true : false);
+
+  useEffect(() => {
+    // Verificar se o usuário está autenticado
+    if (!isSignedIn || !user || !isLoaded) {
+      console.error('Usuário não autenticado.');
+      return;
+    }
+
+    // Verificar se o produto está nos favoritos do usuário
+    setIsFavorite(user.favorites && user.favorites.includes(productId));
+  }, [isSignedIn, user, isLoaded, productId]);
+
   const handleClick = async () => {
     try {
       // Verificar se o usuário está autenticado
@@ -11,6 +25,9 @@ const IconToggle = ({ productId }) => {
         console.error('Usuário não autenticado.');
         return;
       }
+
+      // Verificar se o produto está nos favoritos do usuário
+      const isAlreadyFavorite = user.favorites && user.favorites.includes(productId);
 
       // Enviar uma requisição POST para adicionar/remover o produto dos favoritos
       const response = await fetch(
@@ -22,14 +39,16 @@ const IconToggle = ({ productId }) => {
           },
           body: JSON.stringify({
             clerkUserId: user.id,
-            productId: "65bf9160033f4eb0d2d26458",
+            productId: "65bf90c7033f4eb0d2d263eb",
           }),
         }
       );
 
       if (response.ok) {
         const data = await response.json();
-        setIsFavorite(data.user.favorites.includes(productId));
+        setIsFavorite(!isAlreadyFavorite);
+        setActive(!active);
+        localStorage.setItem('active', !active);
       } else {
         console.error("Erro ao adicionar/remover produto dos favoritos");
       }
@@ -40,11 +59,10 @@ const IconToggle = ({ productId }) => {
 
   return (
     <div>
-      {isFavorite  ? (
-          <img src='https://i.ibb.co/DKmxvXT/heart-3.png' onClick={handleClick} />
-          ) : (
-          <img src='https://i.ibb.co/h1HfgJs/heart-2.png' onClick={handleClick} />
-      )}
+    <div style={{ width: "2rem" }}>
+          <Heart isActive={active} onClick={handleClick} animationScale = {1.25} style = {{marginBottom:'1rem'}} />
+        </div>
+      
     </div>
   );
 };
