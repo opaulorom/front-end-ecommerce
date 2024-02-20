41026ed6e6d350839ed34b69c56b4019a4645f77
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-20 11:39:00 -0300

    add

--- src/components/IconToggle.jsx
+++ src/components/IconToggle.jsx
@@ -4,7 +4,6 @@ import { useUser } from "@clerk/clerk-react";
 const IconToggle = ({ productId }) => {
   const [isFavorite, setIsFavorite] = useState(false);
   const { isSignedIn, user, isLoaded } = useUser();
-
   const handleClick = async () => {
     try {
       // Verificar se o usuário está autenticado
@@ -41,7 +40,7 @@ const IconToggle = ({ productId }) => {
 
   return (
     <div>
-      {isFavorite ? (
+      {isFavorite  ? (
           <img src='https://i.ibb.co/DKmxvXT/heart-3.png' onClick={handleClick} />
           ) : (
           <img src='https://i.ibb.co/h1HfgJs/heart-2.png' onClick={handleClick} />
