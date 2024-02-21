0b7d21d9a97c3e902ddb306d775b1fe416f7bfa1
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-21 09:48:33 -0300

    ADD

--- src/components/IconToggle.jsx
+++ src/components/IconToggle.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Heart from "react-heart";
 import { useUser } from "@clerk/clerk-react";
 
 const IconToggle = ({ productId, isFavorite }) => {
   const [active, setActive] = useState(isFavorite ?? false);
   const { isSignedIn, user, isLoaded } = useUser();
+
+  useEffect(() => {
+    setActive(isFavorite);
+  }, [isFavorite]);
+
   const handleClick = async () => {
     try {
       // Verificar se o usuário está autenticado
@@ -30,7 +35,8 @@ const IconToggle = ({ productId, isFavorite }) => {
 
       if (response.ok) {
         const data = await response.json();
-        setActive(!active);
+        setActive(!active); // Alterar o estado active para o oposto do valor atual
+        localStorage.setItem(`favorite_${productId}`, !active);
       } else {
         console.error("Erro ao adicionar/remover produto dos favoritos");
       }
@@ -39,13 +45,27 @@ const IconToggle = ({ productId, isFavorite }) => {
     }
   };
 
+  useEffect(() => {
+    const storedActive = localStorage.getItem(`favorite_${productId}`);
+    if (storedActive !== null) {
+      setActive(storedActive === "true");
+    }
+  }, [productId]);
+
   return (
     <div>
       <div style={{ width: "2rem" }}>
-        <Heart isActive={active} onClick={handleClick} animationScale={1.25} style={{ marginBottom: '1rem' }} />
+        <Heart
+          isActive={active}
+          onClick={handleClick}
+          animationScale={1.25}
+          inactiveColor="#ccc" // Cor cinza quando inativo
+          activeColor="red" // Cor vermelha quando ativo
+          style={{ marginBottom: '1rem' }}
+        />
       </div>
     </div>
   );
 };
 
-export default IconToggle
+export default IconToggle;
