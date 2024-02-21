300cadfd5ee56b3758839523432393d5281049e0
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-21 10:35:36 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import ProductByCategories from "./ProductByCategories";
 import Home from "./components/Home";
-import DiscountImageLinkPerPercentage from "./components/DiscountImageLinkPerPercentage";
-import BannerWithDiscount from "./components/BannerWithDiscount";
-import DiscountImageLinkPerPercentageAndCategory from "./components/DiscountImageLinkPerPercentageAndCategory";
-import DiscountImageCarousel from "./components/DiscountImageCarousel ";
-
 
 function App() {
- 
   return (
     <>
-    
-<DiscountImageCarousel/> 
-
-      <Home ></Home>
-      <ProductByCategories/>
-      <DiscountImageLinkPerPercentage/> 
-      <BannerWithDiscount/>
-      <DiscountImageLinkPerPercentageAndCategory/>
-
+      <Home></Home>
     </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,16 +4,23 @@ import Navbar from "./Navbar";
 import Header from "./Header";
 import Categories from "./Categories";
 import CategoryCarousel from "./CategoryCarousel";
+import DiscountImageCarousel from "./DiscountImageCarousel ";
+import ProductByCategories from "../ProductByCategories";
+import DiscountImageLinkPerPercentage from "./DiscountImageLinkPerPercentage";
+import BannerWithDiscount from "./BannerWithDiscount";
+import DiscountImageLinkPerPercentageAndCategory from "./DiscountImageLinkPerPercentageAndCategory";
 
 const Home = () => {
-
   return (
     <div>
-      
+      <DiscountImageCarousel />
       <Categories />
       <CategoryCarousel />
       <Header />
-
+      <ProductByCategories />
+      <DiscountImageLinkPerPercentage />
+      <BannerWithDiscount />
+      <DiscountImageLinkPerPercentageAndCategory />
       <Navbar></Navbar>
     </div>
   );
