614797aec2727c8bc37163ffbff55debea53336f
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-25 16:23:44 -0300

    add

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
+import { useUser } from "@clerk/clerk-react";
+import axios from "axios";
 
 const Cart = () => {
+  const [getCart, setGetCart] = useState([]);
+
+  const { isSignedIn, user, isLoaded } = useUser();
+
+  useEffect(() => {
+    if (isLoaded && isSignedIn) {
+      const clerkUserId = user.id;
+      axios
+        .get(`http://localhost:3001/api/cart/${clerkUserId}`)
+        .then((response) => {
+          setGetCart(response.data.cart.products);
+        })
+        .catch((error) => {
+          console.log("Erro ao visualizar frete.", error);
+        });
+    }
+  }, [isLoaded, isSignedIn, user]);
+
   return (
     <div>
-      <Navbar></Navbar>
-      Cart
+      <Navbar />
+      {getCart.map((item) => (
+        <div key={item.productId._id} style={{ marginTop: "2rem", marginLeft: "1rem" }}>
+          <b>nome:</b> {item.productId.name}
+          <b>preço:</b> {item.productId.price}
+          <b>tamanho:</b> {item.productId.size}
+        </div>
+      ))}
     </div>
   );
 };
