d7f4ca7271869edbb5d24f0583efbba1e071d6e7
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-27 09:37:29 -0300

    add

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,11 +3,16 @@ import Navbar from "./Navbar";
 import { useUser } from "@clerk/clerk-react";
 import axios from "axios";
 import Header from "./Header";
-
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Modal from "@mui/joy/Modal";
+import ModalDialog from "@mui/joy/ModalDialog";
+import Typography from "@mui/joy/Typography";
 const Cart = () => {
   const [getCart, setGetCart] = useState([]);
-  const [handleDeleteProduct, setHandleDeleteProduct] = useState(false)
+  const [handleDeleteProduct, setHandleDeleteProduct] = useState(false);
   const { isSignedIn, user, isLoaded } = useUser();
+  const [open, setOpen] = React.useState(false);
 
   useEffect(() => {
     if (isLoaded && isSignedIn) {
@@ -23,18 +28,18 @@ const Cart = () => {
     }
   }, [isLoaded, isSignedIn, user]);
 
-
-
-
-
   const handleDelete = (productId) => {
     const clerkUserId = user.id;
     axios
-      .delete(`http://localhost:3001/api/remove-from-cart/${clerkUserId}/${productId}`)
+      .delete(
+        `http://localhost:3001/api/remove-from-cart/${clerkUserId}/${productId}`
+      )
       .then((response) => {
         console.log(response.data.message);
         // Atualize o estado do carrinho na sua aplicação, se necessário
-        setGetCart((prevCart) => prevCart.filter((item) => item.productId !== productId));
+        setGetCart((prevCart) =>
+          prevCart.filter((item) => item.productId._id !== productId)
+        );
       })
       .catch((error) => {
         console.error("Erro ao remover produto do carrinho:", error);
@@ -165,7 +170,68 @@ const Cart = () => {
               >
                 +
               </button>
-              <button onClick={() => handleDelete(item.productId._id)}>Excluir produto</button>
+              <React.Fragment>
+                <Button
+                  variant="outlined"
+                  color="neutral"
+                  onClick={() => setOpen(true)}
+                >
+                  Open modal
+                </Button>
+                <Modal open={open} onClose={() => setOpen(false)}>
+                  <ModalDialog
+                    aria-labelledby="nested-modal-title"
+                    aria-describedby="nested-modal-description"
+                    sx={(theme) => ({
+                      [theme.breakpoints.only("xs")]: {
+                        top: "unset",
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        borderRadius: 0,
+                        transform: "none",
+                        maxWidth: "unset",
+                      },
+                    })}
+                  >
+                    <Typography id="nested-modal-title" level="h2">
+                      Are you absolutely sure?
+                    </Typography>
+                    <Typography
+                      id="nested-modal-description"
+                      textColor="text.tertiary"
+                    >
+                      This action cannot be undone. This will permanently delete
+                      your account and remove your data from our servers.
+                    </Typography>
+                    <Box
+                      sx={{
+                        mt: 1,
+                        display: "flex",
+                        gap: 1,
+                        flexDirection: { xs: "column", sm: "row-reverse" },
+                      }}
+                    >
+                      <Button
+                        variant="solid"
+                        color="primary"
+                        onClick={() => {
+                          setOpen(false), handleDelete(item.productId._id);
+                        }}
+                      >
+                        continuar
+                      </Button>
+                      <Button
+                        variant="outlined"
+                        color="neutral"
+                        onClick={() => setOpen(false)}
+                      >
+                        Cancelar
+                      </Button>
+                    </Box>
+                  </ModalDialog>
+                </Modal>
+              </React.Fragment>
             </div>
           ))}
         </>
