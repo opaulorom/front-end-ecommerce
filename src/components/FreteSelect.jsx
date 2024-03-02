b60dda9f9ae18b73b30cb4c5c1825ed89e419817
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-03-02 17:52:37 -0300

    add

--- src/components/FreteSelect.jsx
+++ src/components/FreteSelect.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useUser } from "@clerk/clerk-react";
+
 const FreteSelect = () => {
   const [cep, setCep] = useState(localStorage.getItem('cep') || '');
   const [frete, setFrete] = useState(null);
-  const [selectedFreteIndex, setSelectedFreteIndex] = useState(0); // Define o primeiro frete como padrão
+  const [selectedFreteIndex, setSelectedFreteIndex] = useState(localStorage.getItem('selectedFreteIndex') || 0); // Define o primeiro frete como padrão
   const { user } = useUser();
 
   useEffect(() => {
@@ -30,25 +31,26 @@ const FreteSelect = () => {
   useEffect(() => {
     // Atualiza o estado do frete selecionado para o índice do primeiro frete
     if (frete && frete.length > 0) {
-      setSelectedFreteIndex(0);
+      setSelectedFreteIndex(+localStorage.getItem('selectedFreteIndex') || 0);
     }
   }, [frete]);
-
+  
   const handleRadioClick = async (index) => {
     try {
       const clerkUserId = user.id;
       const freteId = frete[index]._id;
-  
+
       // Faz a solicitação PUT para atualizar o valor do frete no carrinho do cliente
       await axios.put(`http://localhost:3001/api/cart/${clerkUserId}/shippingFee/${freteId}`);
-  
+
       // Atualiza o estado do frete selecionado
       setSelectedFreteIndex(index);
+      localStorage.setItem('selectedFreteIndex', index);
     } catch (error) {
       console.error('Error updating shipping fee:', error);
     }
   };
-  
+
   return (
     <div>
       <form>
@@ -62,31 +64,30 @@ const FreteSelect = () => {
       </form>
 
       {frete && (
-  <div>
-    {frete.map((item, index) => (
-      <div key={index}>
-        <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
-          <input
-            type="radio"
-            name="selectedFrete"
-            value={index}
-            onClick={() => handleRadioClick(index)}
-            checked={selectedFreteIndex === index}
-          />
-          <img src={item.logo} alt="logo das transportadoras" style={{ width: "10vw" }} />
-          <p>{item.nomeTransportadora}</p>
-          <p> {item.dataPrevistaEntrega.split('T')[0].split('-').reverse().join('/')}</p>
-          <p> {item.prazoEntrega}</p>
-          <p> {item.valorFrete}</p>
+        <div>
+          {frete.map((item, index) => (
+            <div key={index}>
+              <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+                <input
+                  type="radio"
+                  
+                  name="selectedFrete"
+                  value={index}
+                  onClick={() => handleRadioClick(index)}
+                  checked={selectedFreteIndex === index}
+                />
+                <img src={item.logo} alt="logo das transportadoras" style={{ width: "10vw" }} />
+                <p>{item.nomeTransportadora}</p>
+                <p> {item.dataPrevistaEntrega.split('T')[0].split('-').reverse().join('/')}</p>
+                <p> {item.prazoEntrega}</p>
+                <p> {item.valorFrete}</p>
+              </div>
+            </div>
+          ))}
         </div>
-      </div>
-    ))}
-  </div>
-)}
-
+      )}
     </div>
   );
 };
-
 
 export default FreteSelect;
