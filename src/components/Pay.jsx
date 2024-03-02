581db5bb41f5673cf16018cce276355e1bf9171c
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-03-02 19:20:35 -0300

    add

--- src/components/Pay.jsx
+++ src/components/Pay.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Navbar from "./Navbar";
 import FreteSelect from "./FreteSelect";
 const Pay = () => {
+  const [paymentMethod, setPaymentMethod] = useState('pix');
+
+  const handleChange = (event) => {
+    setPaymentMethod(event.target.value);
+  };
 
   
   return (
@@ -12,6 +17,47 @@ const Pay = () => {
       <div style={{marginTop:"15rem"}}>
         <FreteSelect/>
       </div>
+      <div>
+      <h1>Escolha o método de pagamento:</h1>
+      <div>
+        <input
+          type="radio"
+          id="pix"
+          name="paymentMethod"
+          value="pix"
+          checked={paymentMethod === 'pix'}
+          onChange={handleChange}
+        />
+        <label htmlFor="pix">PIX</label>
+      </div>
+      <div>
+        <input
+          type="radio"
+          id="boleto"
+          name="paymentMethod"
+          value="boleto"
+          checked={paymentMethod === 'boleto'}
+          onChange={handleChange}
+        />
+        <label htmlFor="boleto">Boleto</label>
+      </div>
+      <div>
+        <input
+          type="radio"
+          id="cartao"
+          name="paymentMethod"
+          value="cartao"
+          checked={paymentMethod === 'cartao'}
+          onChange={handleChange}
+        />
+        <label htmlFor="cartao">Cartão de Crédito</label>
+      </div>
+      <div>
+        {paymentMethod === 'pix' && <p>Conteúdo para PIX</p>}
+        {paymentMethod === 'boleto' && <p>Conteúdo para Boleto</p>}
+        {paymentMethod === 'cartao' && <p>Conteúdo para Cartão de Crédito</p>}
+      </div>
+    </div>
     </div>
   );
 };
