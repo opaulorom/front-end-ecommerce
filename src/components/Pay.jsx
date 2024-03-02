36e9b9e41040e3a6d6cc40fd686df9f6aabb88e3
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-03-02 19:37:51 -0300

    add

--- src/components/Pay.jsx
+++ src/components/Pay.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "./Header";
 import Navbar from "./Navbar";
 import FreteSelect from "./FreteSelect";
+
 const Pay = () => {
   const [paymentMethod, setPaymentMethod] = useState('pix');
 
@@ -9,55 +10,69 @@ const Pay = () => {
     setPaymentMethod(event.target.value);
   };
 
+  const handlePixPayment = async () => {
+    try {
+      const clerkUserId = 'user_2d3LzruiZ0VvbjKQYij4QIFh5pv';
+      const response = await fetch(`http://localhost:3001/api/pix/user_2d3LzruiZ0VvbjKQYij4QIFh5pv`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // Adicione aqui o token de acesso, se necessário
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <Header />
       <Navbar />
-      <div style={{marginTop:"15rem"}}>
-        <FreteSelect/>
+    
+      <div style={{ marginTop: "8rem" }}>
+        <h1>Escolha o método de pagamento:</h1>
+        <div>
+          <input
+            type="radio"
+            id="pix"
+            name="paymentMethod"
+            value="pix"
+            checked={paymentMethod === 'pix'}
+            onChange={handleChange}
+          />
+          <label htmlFor="pix">PIX</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="boleto"
+            name="paymentMethod"
+            value="boleto"
+            checked={paymentMethod === 'boleto'}
+            onChange={handleChange}
+          />
+          <label htmlFor="boleto">Boleto</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="cartao"
+            name="paymentMethod"
+            value="cartao"
+            checked={paymentMethod === 'cartao'}
+            onChange={handleChange}
+          />
+          <label htmlFor="cartao">Cartão de Crédito</label>
+        </div>
+        <div>
+          {paymentMethod === 'pix' && <p><button onClick={handlePixPayment}>Pagar com Pix</button></p>}
+          {paymentMethod === 'boleto' && <p>Conteúdo para Boleto</p>}
+          {paymentMethod === 'cartao' && <p>Conteúdo para Cartão de Crédito</p>}
+        </div>
       </div>
-      <div>
-      <h1>Escolha o método de pagamento:</h1>
-      <div>
-        <input
-          type="radio"
-          id="pix"
-          name="paymentMethod"
-          value="pix"
-          checked={paymentMethod === 'pix'}
-          onChange={handleChange}
-        />
-        <label htmlFor="pix">PIX</label>
-      </div>
-      <div>
-        <input
-          type="radio"
-          id="boleto"
-          name="paymentMethod"
-          value="boleto"
-          checked={paymentMethod === 'boleto'}
-          onChange={handleChange}
-        />
-        <label htmlFor="boleto">Boleto</label>
-      </div>
-      <div>
-        <input
-          type="radio"
-          id="cartao"
-          name="paymentMethod"
-          value="cartao"
-          checked={paymentMethod === 'cartao'}
-          onChange={handleChange}
-        />
-        <label htmlFor="cartao">Cartão de Crédito</label>
-      </div>
-      <div>
-        {paymentMethod === 'pix' && <p>Conteúdo para PIX</p>}
-        {paymentMethod === 'boleto' && <p>Conteúdo para Boleto</p>}
-        {paymentMethod === 'cartao' && <p>Conteúdo para Cartão de Crédito</p>}
-      </div>
-    </div>
     </div>
   );
 };
