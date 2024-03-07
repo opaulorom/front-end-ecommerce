a8dea13efea82cb09d40b917488857aee3bd1e22
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-03-07 16:51:20 -0300

    add

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Protected from './Protected'
 import Header from './Header'
+import { useAuth } from "../context/AuthContext";
+import LogoutIcon from "@mui/icons-material/Logout";
+import GoogleLoginButton from './GoogleLoginButton';
 
 
 const Profile = () => {
+  const { logout, loggedIn } = useAuth(); // Obtendo o userId do contexto de autenticação
+  const [showButton, setShowButton] = useState(false);
+  useEffect(() => {
+    if(loggedIn){
+      setShowButton(true)
+    } else {
+      setShowButton(false)
+    }
+  })
   return (
     <div>
       <Header> </Header>
       <Protected/>
-
+      <GoogleLoginButton/>
+      {showButton && (
+        <div className="button" onClick={logout}>
+          <LogoutIcon />
+          <span>Sair</span>
+        </div>
+      )}
       </div>
   )
 }
