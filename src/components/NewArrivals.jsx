e4d422850322335e4e2c1f35e8d6be94f1cfbc8e
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-03-21 19:36:47 -0300

    style: Ajustes na indentação do código.

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -1,75 +1,66 @@
-import React from 'react'
-import Header from './Header'
-import Navbar from './Navbar'
+import React from "react";
+import Header from "./Header";
+import Navbar from "./Navbar";
 
-import  { useEffect, useState } from "react";
-import { Link,  } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from "../context/AuthContext";
 import LogoutIcon from "@mui/icons-material/Logout";
 
 const NewArrivals = () => {
-    const [newArrivals, setNewArrivals] = useState([]);
-    const [totalProducts, setTotalProducts] = useState(0);
-    const [currentPage, setCurrentPage] = useState(1);
-   
-  
-    useEffect(() => {
-      const fetchSearchResults = async () => {
-        try {
-          const response = await fetch(
-            `http://localhost:3001/api/products/new-arrivals?page=${currentPage}`
-          );
-          const data = await response.json();
-          console.log("Data from server:", data);
-          setNewArrivals(data.newArrivals);
-          setTotalProducts(data.totalProducts);
-        } catch (error) {
-          console.error("Erro ao buscar resultados de pesquisa:", error);
-        }
-      };
-  
-      fetchSearchResults();
-    }, [
- 
-      currentPage,
+  const [newArrivals, setNewArrivals] = useState([]);
+  const [totalProducts, setTotalProducts] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
-    ]);
-  
-    const handlePageChange = (event, page) => {
-      setCurrentPage(page);
-    };
-    
-
-
-
-
-
-    const { logout, loggedIn } = useAuth(); // Obtendo o userId do contexto de autenticação
-    const [showButton, setShowButton] = useState(false);
-    useEffect(() => {
-      if (loggedIn) {
-        setShowButton(true);
-      } else {
-        setShowButton(false);
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3001/api/products/new-arrivals?page=${currentPage}`
+        );
+        const data = await response.json();
+        console.log("Data from server:", data);
+        setNewArrivals(data.newArrivals);
+        setTotalProducts(data.totalProducts);
+      } catch (error) {
+        console.error("Erro ao buscar resultados de pesquisa:", error);
       }
-    });
+    };
+
+    fetchSearchResults();
+  }, [currentPage]);
+
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+  };
+
+  const { logout, loggedIn } = useAuth(); // Obtendo o userId do contexto de autenticação
+  const [showButton, setShowButton] = useState(false);
+  useEffect(() => {
+    if (loggedIn) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  });
   return (
     <div>
-         <Header />
+      <Header />
       <Navbar />
-      
 
       <div style={{ marginTop: "10rem" }}>
-        <ul  style={{
+        <ul
+          style={{
             listStyleType: "none",
             padding: 0,
             margin: 0,
             display: "grid",
             gridTemplateColumns: "repeat(4, 1fr)",
             gap: "1rem",
-          }}>
+          }}
+        >
           {newArrivals.map((product) => (
             <li key={product._id}>
               <Link to={`/products/${product._id}`}>
@@ -99,7 +90,7 @@ const NewArrivals = () => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default NewArrivals
+export default NewArrivals;
