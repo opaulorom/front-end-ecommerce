8252d24470b0756dc619fc3bc3fb1fa9cf2491ad
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-04-11 19:18:55 -0300

    feat: adicioana logica do status de pagamento.

--- src/components/AlertComponente.jsx
+++ src/components/AlertComponente.jsx
@@ -1,48 +1,65 @@
 import React, { useEffect, useState } from "react";
 import Cookies from "js-cookie";
-import { useAuth } from '../context/AuthContext';
-import axios from 'axios'; // Corrigindo a importação do Axios
-import styles from "./AlertComponente.module.css"
+import { useAuth } from "../context/AuthContext";
+import axios from "axios"; // Corrigindo a importação do Axios
+import styles from "./AlertComponente.module.css";
 const AlertComponente = () => {
-    const userId = Cookies.get("userId");
-    const { logout, loggedIn } = useAuth();
-    const [orders, setOrders] = useState([]);
+  const userId = Cookies.get("userId");
+  const { logout, loggedIn } = useAuth();
+  const [orders, setOrders] = useState([]);
 
-    useEffect(() => {
-        if (loggedIn) {
-            axios
-                .get(`http://localhost:3001/api/orders/${userId}`)
-                .then((response) => {
-                    setOrders(response.data);
-                    console.log(response.data);
-                })
-                .catch((error) => {
-                    console.error(
-                        "Erro ao obter os pedidos:",
-                        error
-                    );
-                });
-        }
-    }, [loggedIn, userId]);
+  useEffect(() => {
+    if (loggedIn) {
+      axios
+        .get(`http://localhost:3001/api/orders/${userId}`)
+        .then((response) => {
+          setOrders(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("Erro ao obter os pedidos:", error);
+        });
+    }
+  }, [loggedIn, userId]);
 
-    return (
-        //     border-top: 1px solid rgb(206, 212, 218);
-        <div className={styles.ordersContainer}>
-            {orders && orders.map((order, index) => (
-                <div key={index} className={styles.ordersContent}>
-                    <h2>Detalhes do Pedido {index + 1}</h2>
-                    <p>tipo do pagamento: {order.payment.billingType && order.payment.billingType}</p>
-                    <p>valor do pagamento: { order.payment.value && order.payment.value}</p>
-                    <p>{order.payment.installmentNumber && 'numero de parcelas: '}  { order.payment.installmentNumber && order.payment.installmentNumber}</p>
-                    <p>status de pagamento: {
-                     
-                    order.payment.status &&  order.payment.status}</p>
+  return (
+    //     border-top: 1px solid rgb(206, 212, 218);
+    <div className={styles.ordersContainer}>
+      {orders &&
+        orders.map((order, index) => (
+          <div key={index} className={styles.ordersContent}>
+            <h2>Detalhes do Pedido {index + 1}</h2>
+            <p>
+              tipo do pagamento:{" "}
+              {order.payment.billingType && order.payment.billingType}
+            </p>
+            <p>valor: {order.payment.value && order.payment.value}</p>
+            <p>
+              {order.payment.installmentNumber && "numero de parcelas: "}{" "}
+              {order.payment.installmentNumber &&
+                order.payment.installmentNumber}
+            </p>
 
-                    {/* Adicione mais detalhes do pagamento conforme necessário */}
-                </div>
-            ))}
-        </div>
-    );
-}
+            <p>
+              status:{" "}
+              {(() => {
+                if (order.payment.status === "OVERDUE") {
+                  return "Atrasado";
+                } else if (order.payment.status === "PENDING") {
+                  return "Pendente";
+                } else if (order.payment.status === "RECEIVED") {
+                  return "Pago";
+                } else if (order.payment.status === "CONFIRMED") {
+                  return "Cobrança confirmada";
+                } else {
+                  return "Status Desconhecido";
+                }
+              })()}
+            </p>
+          </div>
+        ))}
+    </div>
+  );
+};
 
 export default AlertComponente;
