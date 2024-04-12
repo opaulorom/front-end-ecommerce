be939e230aac3812f8598198608511c2b19e8546
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-04-12 19:37:52 -0300

    refactor: adiciona estado do numero do sininho.

--- src/context/UnreadContext.jsx
+++ src/context/UnreadContext.jsx
@@ -1,13 +1,18 @@
-// UnreadContext.js
-
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const UnreadContext = createContext();
 
 export const useUnreadCount = () => useContext(UnreadContext);
 
 export const UnreadProvider = ({ children }) => {
-  const [unreadCount, setUnreadCount] = useState(0);
+  const [unreadCount, setUnreadCount] = useState(() => {
+    // Tente recuperar o valor do localStorage, se não existir, retorne 0
+    return parseInt(localStorage.getItem('unreadCount')) || 0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('unreadCount', unreadCount);
+  }, [unreadCount]);
 
   const updateUnreadCount = (count) => {
     setUnreadCount(count);
