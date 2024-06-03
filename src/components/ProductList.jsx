1fb0b25e6957d31234a2e01d8e7240337d229ea6
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-06-03 20:40:45 -0300

    refactor: estilisa  os produtos filtrados.

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,8 +4,11 @@ export default function ProductList({ products }) {
   return (
     <ul>
       {products.map(product => (
-        <li key={product.id}>
+        <li key={product.id} style={{
+            border: "2px solid gray"
+        }}>
           {product.name} - Tamanho: {product.size}
+
         </li>
       ))}
     </ul>
