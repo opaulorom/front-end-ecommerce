f0d2f80841483910bbdc46f956976fb606449e86
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-06-13 15:20:15 -0300

    feat: adiciona a logica do icone de ver a senha digitada.

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,7 +4,8 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import ProfileDetails from './ProfileDetails';
 import "./LoginForm.css"
 import { Link } from 'react-router-dom';
-
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 const LoginForm = () => {
   const { loggedIn, isCustomer, login, remainingAttempts } = useAuth();
   const [email, setEmail] = useState('');
@@ -51,7 +52,7 @@ const LoginForm = () => {
           </div>}
           <div className='loginStyle__inputLabel'>
 
-            <label className="email"  htmlFor="email">Email</label>
+            <label className="email" htmlFor="email">Email</label>
             <input
               type="text"
               placeholder="Digite o email..."
@@ -61,36 +62,54 @@ const LoginForm = () => {
                 setFormErrors((prevErrors) => ({ ...prevErrors, email: '' }));
               }}
               className={formErrors.email ? 'error' : ''}
-              
+
             />
             {formErrors.email && <span className='error-message'>{formErrors.email}</span>}
             <br />
-            <label className="password" htmlFor="password">Senha</label>
-            <input
-              type="password"
-              placeholder="Digite a senha..."
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-                setFormErrors((prevErrors) => ({ ...prevErrors, password: '' }));
-              }}
-              className={formErrors.password ? 'error' : ''}
-            />
-    
+            <div style={{
+              position: "relative",
+              display: "flex",
+              flexDirection: "column"
+            }}>
+              <label className="password" htmlFor="password">Senha</label>
+              <input
+                type={password ? "text" : "password"}
+                placeholder="Digite a senha..."
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                  setFormErrors((prevErrors) => ({ ...prevErrors, password: '' }));
+                }}
+                className={formErrors.password ? 'error' : ''}
+              />
+              <div
+                onClick={() => setPassword(!password)}  // Alterna o estado de showPassword
+                style={{
+                  position: "absolute",
+                  right: "10px",
+                  top: "70%",
+                  transform: "translateY(-50%)",
+                  cursor: "pointer"
+                }}
+              >
+                {password ? <VisibilityOffIcon /> : <VisibilityIcon />}
+              </div>
+            </div>
+
 
             <div className='loginStyle__button'>
 
               {formErrors.password && <span className='error-message'>{formErrors.password}</span>}
-          
+
               <button className="loginButton" onClick={handleLogin}>
                 Login
               </button>
               <div className='loginStyle__links'>
                 <span className="span">  Ainda nao tem uma conta  <Link to={"/register"}>Cadastre-se</Link></span>
                 <span className="span"> Esqueceu a senha   <Link to={"/forgotPassword"}>clique aqui</Link>
-</span>
-              
-               
+                </span>
+
+
               </div>
             </div>
           </div>
