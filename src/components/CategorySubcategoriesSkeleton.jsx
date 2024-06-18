301db31d7b34c29ad4d9781b3cd6e2fb4fd1917f
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-06-18 12:36:13 -0300

    feat: adiciona skeleton aos componentes.

---
+++ src/components/CategorySubcategoriesSkeleton.jsx
@@ -0,0 +1,184 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Skeleton from '@mui/material/Skeleton';
+
+export default function CategorySubcategoriesSkeleton() {
+  return (
+
+    <div style={{
+      display: "flex",
+      justifyContent: "space-between"
+    }}>
+      <div>
+        <div >
+          <div style={{
+            marginBottom: "3rem"
+          }}>
+            <Skeleton width={160} height={25} />
+
+          </div>
+
+
+
+
+
+          <div>
+
+            <Skeleton width={160} height={25} />
+            <Skeleton width={90} />
+            <Skeleton width={80} />
+            <Skeleton width={70} />
+            <Skeleton width={90} />
+          </div>
+
+
+
+          <div style={{
+
+            marginTop: "5rem"
+          }}>
+
+            <Skeleton width={160} height={25} />
+            <Skeleton width={90} />
+            <Skeleton width={80} />
+            <Skeleton width={70} />
+            <Skeleton width={90} />
+            <Skeleton width={90} />
+            <Skeleton width={80} />
+            <Skeleton width={70} />
+            <Skeleton width={90} />
+            <Skeleton width={90} />
+            <Skeleton width={80} />
+            <Skeleton width={70} />
+            <Skeleton width={90} />
+          </div>
+
+
+          <div style={{
+
+            marginTop: "5rem"
+          }}>
+            <Skeleton width={160} height={25} />
+
+
+            <div style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(4, 1fr)",
+              gap: "1rem",
+              marginTop: "2rem"
+            }}>
+
+              <Skeleton variant="circular" width={40} height={40} />
+              <Skeleton variant="circular" width={40} height={40} />
+              <Skeleton variant="circular" width={40} height={40} />
+              <Skeleton variant="circular" width={40} height={40} />
+              <Skeleton variant="circular" width={40} height={40} />
+              <Skeleton variant="circular" width={40} height={40} />
+              <Skeleton variant="circular" width={40} height={40} />
+              <Skeleton variant="circular" width={40} height={40} />
+              <Skeleton variant="circular" width={40} height={40} />
+              <Skeleton variant="circular" width={40} height={40} />
+
+            </div>
+          </div>
+          <div style={{
+
+            marginTop: "5rem"
+          }}>
+
+            <Skeleton width={160} height={25} />
+            <Skeleton width={90} />
+            <Skeleton width={80} />
+            <Skeleton width={70} />
+            <Skeleton width={90} />
+          </div>
+        </div>
+      </div>
+      <div>
+
+        <div style={{ display: "flex", justifyContent: "center", gap: "5rem", marginTop: "2rem" }}>
+          <div style={{ textAlign: "center" }}>
+            <Skeleton variant="rectangular" style={{ width: "13vw", height: "40vh" }} />
+            <Skeleton width={80} />
+            <Skeleton width={160} />
+          </div>
+
+          <div style={{ textAlign: "center" }}>
+            <Skeleton variant="rectangular" style={{ width: "13vw", height: "40vh" }} />
+            <Skeleton width={80} />
+            <Skeleton width={160} />
+          </div>
+
+          <div style={{ textAlign: "center" }}>
+            <Skeleton variant="rectangular" style={{ width: "13vw", height: "40vh" }} />
+            <Skeleton width={80} />
+            <Skeleton width={160} />
+          </div>
+
+          <div style={{ textAlign: "center" }}>
+            <Skeleton variant="rectangular" style={{ width: "13vw", height: "40vh" }} />
+            <Skeleton width={80} />
+            <Skeleton width={160} />
+          </div>
+        </div>
+
+
+        <div style={{ display: "flex", justifyContent: "center", gap: "5rem", marginTop: "8rem" }}>
+          <div style={{ textAlign: "center" }}>
+            <Skeleton variant="rectangular" style={{ width: "13vw", height: "40vh" }} />
+            <Skeleton width={80} />
+            <Skeleton width={160} />
+          </div>
+
+          <div style={{ textAlign: "center" }}>
+            <Skeleton variant="rectangular" style={{ width: "13vw", height: "40vh" }} />
+            <Skeleton width={80} />
+            <Skeleton width={160} />
+          </div>
+
+          <div style={{ textAlign: "center" }}>
+            <Skeleton variant="rectangular" style={{ width: "13vw", height: "40vh" }} />
+            <Skeleton width={80} />
+            <Skeleton width={160} />
+          </div>
+
+          <div style={{ textAlign: "center" }}>
+            <Skeleton variant="rectangular" style={{ width: "13vw", height: "40vh" }} />
+            <Skeleton width={80} />
+            <Skeleton width={160} />
+          </div>
+        </div>
+
+
+        <div style={{ display: "flex", justifyContent: "center", gap: "5rem", marginTop: "8rem" }}>
+          <div style={{ textAlign: "center" }}>
+            <Skeleton variant="rectangular" style={{ width: "13vw", height: "40vh" }} />
+            <Skeleton width={80} />
+            <Skeleton width={160} />
+          </div>
+
+          <div style={{ textAlign: "center" }}>
+            <Skeleton variant="rectangular" style={{ width: "13vw", height: "40vh" }} />
+            <Skeleton width={80} />
+            <Skeleton width={160} />
+          </div>
+
+          <div style={{ textAlign: "center" }}>
+            <Skeleton variant="rectangular" style={{ width: "13vw", height: "40vh" }} />
+            <Skeleton width={80} />
+            <Skeleton width={160} />
+          </div>
+
+          <div style={{ textAlign: "center" }}>
+            <Skeleton variant="rectangular" style={{ width: "13vw", height: "40vh" }} />
+            <Skeleton width={80} />
+            <Skeleton width={160} />
+          </div>
+        </div>
+
+      </div>
+
+
+    </div>
+  );
+}
