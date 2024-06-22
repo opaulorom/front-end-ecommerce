abdac025aa119e9b36dd36e3e8ac928858cfcc9b
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-06-22 16:40:45 -0300

    refactor: refatora estilisação dos cards.

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,9 +4,7 @@ export default function ProductList({ products }) {
   return (
     <ul className={styles.productListContainer}>
       {products.map(product => (
-        <li key={product.id} style={{
-          border: "2px solid gray"
-        }} className={styles.productListContainer__productcard}>
+        <li key={product.id} className={styles.productListContainer__productcard}>
           <img src={product.variations[0].urls[0]} alt={product.name} className={styles.productListContainer__image} />
           <p className={styles.productListContainer__price}>Preço: R${product.variations[0].sizes[0].price.toFixed(2)}</p>
 
