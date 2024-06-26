e54c288113b9cfa35c1e64783ae7677e12c9998c
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-06-26 18:43:12 -0300

    refactor: refaz a logica de paginação

--- src/components/MyOrders.jsx
+++ src/components/MyOrders.jsx
@@ -1,91 +1,74 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; 
 import Header from "./Header";
 import Navbar from "./Navbar";
 import { useAuth } from "../context/AuthContext";
 import Cookies from "js-cookie";
 import axios from "axios";
 import CircularIndeterminate from "./CircularIndeterminate";
+import { Pagination, Stack } from "@mui/material";
 
 const MyOrders = () => {
   const userId = Cookies.get("userId");
-  const { loggedIn } = useAuth();
+  const { logout, loggedIn } = useAuth();
   const [boletos, setBoletos] = useState([]);
   const [pix, setPix] = useState([]);
   const [creditCard, setCreditCard] = useState([]);
-
   const credentials = Cookies.get('role');
+  const [totalOrders, setTotalOrders] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+
   const token = Cookies.get('token');
+  const [expanded, setExpanded] = useState({});
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    if (loggedIn) {
-      axios
-        .get(`http://localhost:3001/api/allOrders/boleto/${userId}`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .then((response) => {
-          setBoletos(response.data);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.error("Erro ao obter os pedidos:", error);
-          setLoading(false);
-        });
-    }
-  }, [loggedIn, userId, token, credentials]);
-
-
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded((prevExpanded) => ({
+      ...prevExpanded,
+      [panel]: newExpanded ? panel : false,
+    }));
+  };
 
   useEffect(() => {
     if (loggedIn) {
       axios
-        .get(`http://localhost:3001/api/allOrders/creditCard/${userId}`, {
+        .get(`http://localhost:3001/api/allOrders/${userId}`, {
           headers: {
             Authorization: `Bearer ${token}`,
+            Credentials: credentials,
           },
+          params: { page: currentPage, pageSize },
         })
         .then((response) => {
-          setCreditCard(response.data);
+          setBoletos(response.data.boleto);
+          setPix(response.data.pix);
+          setCreditCard(response.data.creditCard);
+          setTotalOrders(response.data.totalOrders);
           setLoading(false);
         })
         .catch((error) => {
           console.error("Erro ao obter os pedidos:", error);
-          setLoading(false);
         });
     }
-  }, [loggedIn, userId, token, credentials]);
+  }, [loggedIn, userId, currentPage, pageSize]);
 
+  const handleClick = (payload) => {
+    navigator.clipboard.writeText(payload);
+  };
+  const [activeTab, setActiveTab] = useState(0);
 
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
 
+  const tabStyle = {
+    color: "#ccc",
+  };
 
-  useEffect(() => {
-    if (loggedIn) {
-      axios
-        .get(`http://localhost:3001/api/allOrders/pix/${userId}`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .then((response) => {
-          setPix(response.data);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.error("Erro ao obter os pedidos:", error);
-          setLoading(false);
-        });
-    }
-  }, [loggedIn, userId, token, credentials]);
-
-
-
-
-
-
-
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+  };
 
   return (
     <>
@@ -99,19 +82,15 @@ const MyOrders = () => {
             marginTop: "20rem",
           }}
         >
-          <CircularIndeterminate />
+          <CircularIndeterminate />;
         </div>
       ) : (
         <>
-
-
-
-{pix &&
-          pix.map((order, index) => (
+          {[...boletos, ...pix, ...creditCard].map((order, index) => (
             <div
               key={index}
               style={{
-                position: "reative",
+                position: "relative",
                 display: "flex",
                 margin: "0 auto",
                 marginTop: "15rem",
@@ -197,207 +176,25 @@ const MyOrders = () => {
               </div>
             </div>
           ))}
-
-
-
-          {boletos.length > 0 ? (
-            boletos.map((order) => (
-              <div
-                key={order._id}
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  marginTop: "15rem",
-                  marginBottom: "15rem",
-                  border: "1px solid rgba(0, 0, 0, 0.10)",
-                  width: "60vw",
-                  padding: "1rem",
-                  position: "relative",
-                }}
-              >
-                <div style={{ flex: 1 }}>
-                  {order.products.map((product, prodIndex) => (
-                    <div key={prodIndex} style={{ marginBottom: "1rem" }}>
-                      <Link to={`/order/${order.customerId}/${order._id}`}>
-                        <img
-                          src={product.image}
-                          alt={`Produto ${product.productId}`}
-                          style={{ width: "8vw" }}
-                        />
-                      </Link>
-                      <div>
-                        <span>{product.name}</span>
-                        <span> - </span>
-                        <span>{product.size}</span>
-                        <span> - </span>
-                        <span>{product.color}</span>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                <div
-                  style={{
-                    flex: 1,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "flex-end",
-                    padding: "1rem",
-                  }}
-                >
-                  <div>
-                    <span>Transportadora:</span>
-                    <img
-                      src={order.shippingFeeData.logo}
-                      alt={order.shippingFeeData.transportadora}
-                      style={{ width: "5rem", marginLeft: "1rem" }}
-                    />
-                  </div>
-                  <div>
-                    <span>Frete: R$ {order.shippingFeeData.shippingFeePrice}</span>
-                  </div>
-                  <div>
-                    <span>Tipo de Pagamento:</span>
-                    <span>{order.billingType}</span>
-                  </div>
-                  <div>
-                    <span>Total: R$ {order.value.toFixed(2)}</span>
-                  </div>
-                  <div>
-                    <span>Status:</span>
-                    <span>
-                      {(() => {
-                        switch (order.status) {
-                          case "RECEIVED":
-                            return "pago";
-                          case "CONFIRMED":
-                            return "Cobrança confirmada";
-                          case "PENDING":
-                            return "Pendente";
-                          case "OVERDUE":
-                            return "Cobrança vencida";
-                          default:
-                            return "";
-                        }
-                      })()}
-                    </span>
-                  </div>
-                  <div>
-                    <span>Código de Rastreio:</span>
-                    <span>{order.trackingCode}</span>
-                  </div>
-                  <div>
-                    <a href={order.bankSlipUrl} target="_blank" rel="noopener noreferrer">
-                      Ver boleto
-                    </a>
-                  </div>
-                </div>
-              </div>
-            ))
-          ) : (
-            <div style={{ textAlign: "center", marginTop: "1rem" }}>
-              <p>Nenhum pedido encontrado.</p>
-            </div>
-          )}
-
-
-
-
-{creditCard &&
-          creditCard.slice(0, 1).map((order, index) => (
-            <div
-              key={index}
-              style={{
-                position: "reative",
-                display: "flex",
-                margin: "0 auto",
-                marginTop: "15rem",
-                marginBottom: "15rem",
-                border: "1px solid rgba(0, 0, 0, 0.10)",
-                width: "60vw",
-                height: "50vh",
-              }}
-            >
-              <div style={{ padding: "7rem 3rem" }}>
-                {order.products.slice(0, 1).map((product, prodIndex) => (
-                  <div
-                    key={prodIndex}
-                    style={{
-                      marginBottom: "10rem",
-                    }}
-                  >
-                    <Link to={`/order/${order.custumerId}/${order._id}`}>
-                      <img
-                        src={product.image}
-                        alt={`Produto ${product.productId}`}
-                        style={{ width: "8vw" }}
-                      />
-                    </Link>
-                  </div>
-                ))}
-              </div>
-              <div
-                style={{
-                  position: "absolute",
-                  right: "22rem",
-                  marginTop: "3rem",
-                  gap: "2rem",
-                }}
-              >
-                {" "}
-                <span>
-                  {order.billingType === "CREDIT_CARD" && "Cartão de Crédito"}
-                </span>
-                <span>{order.value}</span>{" "}
-              </div>
-
-              <div
-                style={{
-                  position: "absolute",
-                  right: "22rem",
-                  marginTop: "8rem",
-                }}
-              ></div>
-              <div
-                style={{
-                  position: "absolute",
-                  right: "22rem",
-                  marginTop: "5rem",
-                }}
-              >
-                <span>Status</span>
-                <span>
-                  {" "}
-                  {(() => {
-                    switch (order.status) {
-                      case "RECEIVED":
-                        return "pago";
-                      case "CONFIRMED":
-                        return "Cobrança confirmada";
-                      case "PENDING":
-                        return "Pendente";
-                      case "OVERDUE":
-                        return "Cobrança vencida";
-                      default:
-                        return;
-                    }
-                  })()}
-                </span>
-              </div>
-
-              <div
-                style={{
-                  position: "absolute",
-                  right: "22rem",
-                  marginTop: "15rem",
-                }}
-              >
-                <div>
-                  <span>código de rastreio</span>
-                  <span>{order.trackingCode}</span>
-                </div>
-              </div>
-            </div>
-          ))}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginBottom: "2rem",
+            }}
+          >
+            <Stack spacing={2}>
+              <Pagination
+                count={Math.ceil(totalOrders / pageSize)}
+                variant="outlined"
+                color="primary"
+                size="large"
+                page={currentPage}
+                onChange={handlePageChange}
+              />
+            </Stack>
+          </div>
         </>
       )}
     </>
