da837a38ba5960692de2c48ffe0d471ff7ef4626
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-08 19:59:55 -0300

    feat: adiciona footer.

--- src/components/SliderSkeleton.jsx
+++ src/components/SliderSkeleton.jsx
@@ -4,7 +4,7 @@ import Skeleton from '@mui/material/Skeleton';
 
 export default function SliderSkeleton() {
   return (
-    <Box sx={{ width: 800}}>
+    <Box sx={{ width: 100}}>
       <Skeleton sx={{ height: 500}} animation="wave" />
      
     </Box>
