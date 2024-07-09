5981899045e6b94eb9d9fe95d67c403fb9db88cb
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-09 13:41:36 -0300

    refactor: estilisa home do site.

--- src/components/SliderSkeleton.jsx
+++ src/components/SliderSkeleton.jsx
@@ -5,7 +5,16 @@ import styles from "./SliderSkeleton.module.css"
 export default function SliderSkeleton() {
   return (
     <div  className={styles.SliderSkeletonContainer}>
-      <Skeleton sx={{ height: 300, width: '90%', borderRadius:"8px", marginTop:"-12rem" }}  animation="wave" className={styles.SliderSkeletonContainer__Skeleton}/>
+      <Skeleton   sx={{
+          height: 300,
+          width: '90%',
+          borderRadius: "8px",
+          marginTop: "-12rem",
+          '@media (min-width: 700px)': {
+            height: 500,
+            marginTop: "-13rem",
+          },
+        }}  animation="wave" className={styles.SliderSkeletonContainer__Skeleton}/>
      
     </div>
   );
