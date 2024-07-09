8f7f98ce717631970c58f19fd065dc90a0d629c0
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-09 20:37:38 -0300

    refactor: estilisa home do site.

--- src/components/NewArrivalsSkeleton.jsx
+++ src/components/NewArrivalsSkeleton.jsx
@@ -1,42 +1,85 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Skeleton from '@mui/material/Skeleton';
+import * as React from "react";
+import Skeleton from "@mui/material/Skeleton";
+import { Stack } from "@mui/material";
 
 export default function NewArrivalsSkeleton() {
   return (
-    <div style={{
-      display: "grid",
-      gridTemplateColumns: "repeat(2, 1fr)", /* Define duas colunas com largura igual */
-      justifyContent: "center", /* Centraliza as colunas horizontalmente */
-      gap: "1rem", /* Espaçamento entre os elementos */
-   
-  '@media (max-width: 768px)': {
-    gridTemplateColumns: "1fr", /* Altera para uma coluna quando a largura for menor que 768px */
-  }
-    }}>
+    <Stack 
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "repeat(2, 1fr)" /* Colunas responsivas */,
+        justifyContent: "center" /* Centraliza as colunas horizontalmente */,
+        gap: "1rem" /* Espaçamento entre os elementos */,
+        "@media (min-width: 700px)": {
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)" /* Colunas responsivas */,
+          justifyContent: "center" /* Centraliza as colunas horizontalmente */,
+        },
+      }}
+    >
       <div style={{ textAlign: "center", width: "100%" }}>
-        <Skeleton variant="rectangular" style={{ width: "100%", height: "30vh" }} />
-        <Skeleton width={80} />
-        <Skeleton width={160} />
-      </div>
-      
-      <div style={{ textAlign: "center", width: "100%" }}>
-        <Skeleton variant="rectangular" style={{ width: "100%", height: "30vh" }} />
+        <Skeleton
+          variant="rectangular"
+          sx={{
+            width: "100%",
+            height: "30vh",
+            "@media (min-width: 700px)": {
+              width: "13vw",
+              height: "40vh",
+            },
+          }}
+        />
         <Skeleton width={80} />
         <Skeleton width={160} />
       </div>
 
       <div style={{ textAlign: "center", width: "100%" }}>
-        <Skeleton variant="rectangular" style={{ width: "100%", height: "30vh" }} />
+        <Skeleton
+          variant="rectangular"
+          sx={{
+            width: "100%",
+            height: "30vh",
+            "@media (min-width: 700px)": {
+              width: "13vw",
+              height: "40vh",
+            },
+          }}
+        />
         <Skeleton width={80} />
         <Skeleton width={160} />
       </div>
 
       <div style={{ textAlign: "center", width: "100%" }}>
-        <Skeleton variant="rectangular" style={{ width: "100%", height: "30vh" }} />
+        <Skeleton
+          variant="rectangular"
+          sx={{
+            width: "100%",
+            height: "30vh",
+            "@media (min-width: 700px)": {
+              width: "13vw",
+              height: "40vh",
+            },
+          }}
+        />
         <Skeleton width={80} />
         <Skeleton width={160} />
       </div>
-    </div>
+
+      <div style={{ textAlign: "center", width: "100%" }}>
+        <Skeleton
+          variant="rectangular"
+          sx={{
+            width: "100%",
+            height: "30vh",
+            "@media (min-width: 700px)": {
+              width: "13vw",
+              height: "40vh",
+            },
+          }}
+        />
+        <Skeleton width={80} />
+        <Skeleton width={160} />
+      </div>
+    </Stack >
   );
 }
