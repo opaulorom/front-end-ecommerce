3b8e32bc259e79f506010cbf8362a317cb72ee7f
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-15 11:12:40 -0300

    refactor: estilisa recuperação de email.

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import { Link, useNavigate } from 'react-router-dom';
-import SliderSkeleton from './SliderSkeleton';
-import './Slider.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { Link, useNavigate } from "react-router-dom";
+import SliderSkeleton from "./SliderSkeleton";
+import "./Slider.css";
 
-const Slider = ({ alt, imageWidth, imageHeight, autoPlayInterval, dataFetch }) => {
+const Slider = ({
+  alt,
+  imageWidth,
+  imageHeight,
+  autoPlayInterval,
+  dataFetch,
+}) => {
   const [categories, setCategories] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const navigate = useNavigate();
@@ -16,16 +22,21 @@ const Slider = ({ alt, imageWidth, imageHeight, autoPlayInterval, dataFetch }) =
     const fetchData = async () => {
       try {
         // await new Promise((resolve) => setTimeout(resolve, 10000));
-        const response = await axios.get('http://localhost:3001/api/categories');
-        console.log('Categories Response:', response.data);
+        const response = await axios.get(
+          "http://localhost:3001/api/categories"
+        );
+        console.log("Categories Response:", response.data);
 
-        if (response.data.categories && Array.isArray(response.data.categories)) {
+        if (
+          response.data.categories &&
+          Array.isArray(response.data.categories)
+        ) {
           setCategories(response.data.categories.slice(0, 3)); // Mostrar apenas as primeiras três categorias
         } else {
           setCategories([]);
         }
       } catch (error) {
-        console.error('Error fetching categories:', error);
+        console.error("Error fetching categories:", error);
       } finally {
         setLoading(false);
       }
@@ -49,7 +60,7 @@ const Slider = ({ alt, imageWidth, imageHeight, autoPlayInterval, dataFetch }) =
   };
 
   return (
-    <div style={{ position: 'relative', textAlign: 'center' }}>
+    <div style={{ position: "relative", textAlign: "center" }}>
       {loading ? (
         <div style={{ marginTop: "15rem" }}>
           <SliderSkeleton />
@@ -57,45 +68,76 @@ const Slider = ({ alt, imageWidth, imageHeight, autoPlayInterval, dataFetch }) =
       ) : (
         <>
           {categories.map((category, index) => (
-            <div key={index} style={{ display: index === currentIndex ? 'block' : 'none' }}>
+            <div
+              key={index}
+              style={{ display: index === currentIndex ? "block" : "none" }}
+            >
               {category.slider.map((subcategoryImages, subIndex) => (
                 <div key={subIndex}>
                   {subcategoryImages.map((image, imageIndex) => (
-                    <div key={imageIndex} style={{ display: 'inline-block', margin: '10px' }}>
-                      <Link to={`/categories/${encodeURIComponent(category.name)}`} >
-                        <img src={image.imageUrl} alt={`Image ${image._id}`} className="categoriesImageStyle" />
-
+                    <div
+                      key={imageIndex}
+                      style={{ display: "inline-block", margin: "10px" }}
+                    >
+                      <Link
+                        to={`/categories/${encodeURIComponent(category.name)}`}
+                      >
+                        <img
+                          src={image.imageUrl}
+                          alt={`Image ${image._id}`}
+                          className="categoriesImageStyle"
+                        />
                       </Link>
-
                     </div>
                   ))}
-
                 </div>
               ))}
-              <div className="indicator-container" style={{ textAlign: 'center', marginTop: '10px' }}>
+              <div
+                className="indicator-container"
+                style={{ textAlign: "center", marginTop: "10px" }}
+              >
                 {categories.map((_, index) => (
                   <span
                     key={index}
-                    className={`indicator ${index === currentIndex ? 'active' : ''}`}
+                    className={`indicator ${
+                      index === currentIndex ? "active" : ""
+                    }`}
                     onClick={() => handleIndicatorClick(index)}
-                    style={{  background: index === currentIndex ? '#000' : '#999' }}
-                    
+                    style={{
+                      background: index === currentIndex ? "#000" : "#999",
+                    }}
                   ></span>
                 ))}
               </div>
             </div>
           ))}
-          <div className='arrowContainer'>
-            <div className="arrowIcon"
-              style={{ position: 'absolute', top: '55%', transform: 'translateY(-50%)', left: "50px", cursor: 'pointer', color: "black", fontSize: '24px', }} onClick={() => setCurrentIndex((prevIndex) => (prevIndex - 1 + categories.length) % categories.length)} >
-              <ArrowBackIosIcon style={{ fontSize: '2rem' }} />
+          <div className="arrowContainer">
+            <div
+              className="arrowIconLeft"
+            
+              onClick={() =>
+                setCurrentIndex(
+                  (prevIndex) =>
+                    (prevIndex - 1 + categories.length) % categories.length
+                )
+              }
+            >
+              <ArrowBackIosIcon style={{ fontSize: "2rem" }} />
             </div>
-            <div className="arrowIcon" style={{ position: 'absolute', top: '55%', transform: 'translateY(-50%)', right: "60px", cursor: 'pointer', color: "black", fontSize: '24px' }} onClick={() => setCurrentIndex((prevIndex) => (prevIndex + 1) % categories.length)}>
-              <ArrowForwardIosIcon style={{ fontSize: '2rem', position: "absolute", }} />
+            <div
+              className="arrowIconRight"
+             
+              onClick={() =>
+                setCurrentIndex(
+                  (prevIndex) => (prevIndex + 1) % categories.length
+                )
+              }
+            >
+              <ArrowForwardIosIcon
+                style={{ fontSize: "2rem", position: "absolute" }}
+              />
             </div>
-
           </div>
-
         </>
       )}
     </div>
