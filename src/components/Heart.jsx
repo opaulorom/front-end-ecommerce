d443b1b6f9c3a35894f304a9537f3bdc6caa7fa7
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-30 12:35:14 -0300

    refactor: estiliza componente de favoritos.

--- src/components/Heart.jsx
+++ src/components/Heart.jsx
@@ -1,24 +1,24 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import {useAuth} from "../context/AuthContext"
+import { useAuth } from "../context/AuthContext";
 import Cookies from "js-cookie";
 import Navbar from "./Navbar";
 import Header from "./Header";
+import styles from "./Heart.module.css"; // Importa o módulo CSS
 
 const Heart = () => {
   const [favorites, setFavorites] = useState([]);
 
-  const userId = Cookies.get('userId'); // Obtenha o token do cookie
-  const { logout, loggedIn } = useAuth(); // Obtendo o userId do contexto de autenticação
-  const credentials = Cookies.get('role'); // Obtenha as credenciais do cookie
-  const token = Cookies.get('token'); // Obtenha o token do cookie
+  const userId = Cookies.get("userId");
+  const { logout, loggedIn } = useAuth();
+  const credentials = Cookies.get("role");
+  const token = Cookies.get("token");
+
   useEffect(() => {
     if (loggedIn) {
-
       axios
-        .get(`http://localhost:3001/api/favorites/${userId}`,
-        {
+        .get(`http://localhost:3001/api/favorites/${userId}`, {
           headers: {
             Authorization: `Bearer ${token}`,
             Credentials: credentials,
@@ -33,28 +33,30 @@ const Heart = () => {
     }
   }, [loggedIn, userId]);
 
+  const charLimit = 24;
+
   return (
-    <div>
-      <Header/>
-      <Navbar/>
-      
-      {favorites.length === 0 && !loggedIn && (
-        <div  style={{display:"flex",alignItems:"center", justifyContent:"center", marginTop:"15rem",  }}>
-                <div style={{ marginTop:"5rem", fontFamily:"poppins", fontSize:"1rem", fontWeight:"400" }}> Somente os usuários registrados podem acessar esta página faça <Link to={"/perfil"} style={{ color: "inherit", textDecoration: "none", }}> <b style={{fontFamily:"poppins", fontWeight:"600", fontSize:"1.2rem" }}>  Login</b>.</Link> </div>
-
-
-        </div>
-   
-      )}
-      <ul>
+    <div className={styles.HeartContainer}>
+      <Header />
+      <Navbar />
+      <ul className={styles.HeartUL}>
         {favorites.map((favorite) => (
-          <div key={favorite._id}>
-            <Link to={`/products/${favorite._id}`}>
-              <img src={favorite.variations[0].urls[0]} alt="" />
-              <li>{favorite.name}</li>
+          <div key={favorite._id} className={styles.Heartfavorite}>
+            <Link to={`/products/${favorite._id}`} className={styles.HeartLink}>
+              <img
+                src={favorite.variations[0].urls[0]}
+                alt=""
+                className={styles.HeartIMG}
+              />
+              <li className={styles.Name}>
+                {favorite.name.length > charLimit
+                  ? favorite.name.substring(0, charLimit) + "..."
+                  : favorite.name}
+              </li>
             </Link>
           </div>
         ))}
+
       </ul>
     </div>
   );
