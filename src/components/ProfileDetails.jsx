65e88b50c804690866556d460bba4d58876d4712
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-08-24 16:33:30 -0300

    refactor: adiciona  google analytics

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import Home from './Home'
 import Protected from './Protected'
 import Header from './Header'
+import { useLocation } from 'react-router-dom'
+import { logPageView } from '../../analytics'
 
 const ProfileDetails = () => {
+  const location = useLocation();
+  useEffect(() => {
+    logPageView();
+  }, [location]);
   return (
     <div>
         <Header></Header>
