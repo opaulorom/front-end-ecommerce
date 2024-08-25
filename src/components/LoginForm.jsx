3e8cc8ee9a139bc95516f921d7c6b6df27c9bb4f
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-08-25 20:41:30 -0300

    refactor: adiciona  google analytics

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import LogoutIcon from '@mui/icons-material/Logout';
 import ProfileDetails from './ProfileDetails';
