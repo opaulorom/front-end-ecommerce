b2ae0be0595ec562e8de03a2b5a142e25ab34dc0
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-08-29 18:41:39 -0300

    refactor: refatora logica verificação de caracteres

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,141 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useLocation, useParams } from 'react-router-dom';
-import Header from './Header';
-import Navbar from './Navbar';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import CheckIcon from '@mui/icons-material/Check';
-import CloseIcon from '@mui/icons-material/Close';
-import styles from './Register.module.css';
-import { useConfig } from '../context/ConfigContext';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Navbar from "./Navbar";
+import Header from "./Header";
+import styles from "./RegisterLink.module.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { logPageView } from '../../analytics';
+import { useConfig } from "../context/ConfigContext";
+import { useLocation } from "react-router-dom";
+import { logPageView } from "../../analytics";
 
-function RegisterUser() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
-  const [error, setError] = useState('');
-  const { token } = useParams();
-  const [showPassword, setShowPassword] = useState(null);
-  const [containsSpecialCharacter, setContainsSpecialCharacter] = useState(false);
-  const [containsNumber, setContainsNumber] = useState(false);
-  const [containsCapitalLetter, setContainsCapitalLetter] = useState(false);
-  const [containsLowerCase, setContainsLowerCase] = useState(false);
-  const [isAddButtonDisabled, setIsAddButtonDisabled] = useState(true);
+const RegisterLink = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const { apiUrl } = useConfig();
-
-  useEffect(() => {
-    // O token agora está disponível aqui, você pode usá-lo como desejar
-    console.log(token);
-  }, [token]);
-
-  useEffect(() => {
-    // Verifica se todos os critérios estão atendidos
-    if (containsSpecialCharacter && containsNumber && containsCapitalLetter && containsLowerCase) {
-      setIsAddButtonDisabled(false);
-    } else {
-      setIsAddButtonDisabled(true);
-    }
-  }, [containsSpecialCharacter, containsNumber, containsCapitalLetter, containsLowerCase]);
-
-  const handlePasswordChange = (e) => {
-    const newPassword = e.target.value;
-    setPassword(newPassword);
-
-    // Verifica se há algum caractere especial na senha
-    const specialCharacterPattern = /[!@#$%^&*(),.?":{}|<>]/;
-    setContainsSpecialCharacter(specialCharacterPattern.test(newPassword));
-
-    const OneNumber = /[0-9]/;
-    setContainsNumber(OneNumber.test(newPassword));
-
-    const OneCapitalLetter = /[A-Z]/;
-    setContainsCapitalLetter(OneCapitalLetter.test(newPassword));
-
-    const OneLowerCase = /[a-z]/;
-    setContainsLowerCase(OneLowerCase.test(newPassword));
-  };
-
-  const role = 'customer'; // Definindo o papel (role) como 'customer' por padrão
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(`${apiUrl}/register/${token}`, { email, password, role });
-      setMessage(response.data.message);
-      toast.success("Usuário registrado com sucesso.");
-
-    } catch (error) {
-      setError(error.response.data.error);
-    }
-  };
   const location = useLocation();
 
   useEffect(() => {
     logPageView();
   }, [location]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const newUser = { email, password, role: "customer" };
+      await axios.post(`${apiUrl}/register/request`, newUser);
+      
+      // Limpar o formulário após o envio bem-sucedido
+      setEmail("");
+      setPassword("");
+      setError("");
+      
+      // Exibir mensagem de sucesso
+      toast.success("Email enviado com sucesso! Verifique sua caixa de email.");
+      
+    } catch (err) {
+      if (err.response && err.response.status === 400) {
+        // Email já cadastrado
+        toast.error("Este email já está cadastrado. Por favor, use outro email.");
+      } else {
+        // Outro erro
+        toast.error("Ocorreu um erro ao enviar o email. Tente novamente.");
+      }
+      setError(err.response?.data.message || "Ocorreu um erro.");
+    }
+  };
+
   return (
     <>
       <Header />
       <Navbar />
-      <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-          style={{ marginTop: "8rem" }}
-        />
-      <div className={styles.container}>
-        <form onSubmit={handleSubmit} className={styles.formContainer}>
-          <h2 className={styles.h2}>Cadastro</h2>
-          {message && <p>{message}</p>}
-          <div className={styles.specs}>
-            <label className={styles.label}>Email:</label>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required className={styles.input} />
+
+      <div className={styles.formContainer}>
+        <h1 className={styles.formContainer__h1}>Digite seu email</h1>
+
+        
+        <form onSubmit={handleSubmit} className={styles.formContainer__form}>
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+            style={{ marginTop: "8rem" }}
+          />
+          <label className={styles.formContainer__label}>Email:</label>
+          <div className={styles.formContainer__div}>
+            <input
+              type="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className={styles.formContainer__input}
+              placeholder="email@exemplo.com"
+            />
+            <button type="submit" className={styles.formContainer__button}>Enviar</button>
           </div>
-          {error && <p>{error}</p>}
-          <div>
-            <label  className={styles.label}>Password:</label>
-            <div style={{ position: "relative" }}>
-              <input type={showPassword ? "text" : "password"} value={password} onChange={handlePasswordChange} required className={styles.input} />
-              <div onClick={() => setShowPassword(!showPassword)} 
-              className={styles.VisibilityIcon}
-              >
-                {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-              </div>
-            </div>
-          </div>
-          <div className={styles.specs}>
-            <h4  className={styles.label}>Sua senha precisa conter:</h4>
-            <span style={{ color: containsSpecialCharacter ? 'green' : 'black' }} className={styles.span}>
-              {containsSpecialCharacter ? <CheckIcon /> : <CloseIcon style={{ color: "#ee0d1f" }} />} 1 caractere especial
-            </span>
-            <span style={{ color: containsCapitalLetter ? 'green' : 'black' }} className={styles.span}>
-              {containsCapitalLetter ? <CheckIcon /> : <CloseIcon style={{ color: "#ee0d1f" }} />} 1 letra maiúscula
-            </span>
-            <span style={{ color: containsLowerCase ? 'green' : 'black' }}   className={styles.span}>
-              {containsLowerCase ? <CheckIcon /> : <CloseIcon style={{ color: "#ee0d1f" }} />} 1 letra minúscula
-            </span>
-            <span style={{ color: containsNumber ? 'green' : 'black' }} className={styles.span}>
-              {containsNumber ? <CheckIcon /> : <CloseIcon style={{ color: "#ee0d1f" }} />} 1 número
-            </span>
-          </div>
-          <button type="submit" style={{ backgroundColor: isAddButtonDisabled ? "#ccc" : "#14337C" }} disabled={isAddButtonDisabled} className={styles.formContainer__button}>
-            Cadastrar
-          </button>
         </form>
       </div>
     </>
   );
-}
+};
 
-export default RegisterUser;
+export default RegisterLink;
