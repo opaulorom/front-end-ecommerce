9547c37d44796093bb9c19ce86989a59290ce280
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-04 19:17:00 -0300

    feat: adiciona logica do frete gratis

--- src/context/ConfigContext.jsx
+++ src/context/ConfigContext.jsx
@@ -1,12 +1,12 @@
 // ConfigContext.js
 import React, { createContext, useState } from 'react';
 const localhost = 'http://localhost:3001'
-
+const prodURL = 'https://serveradmin-whhj.onrender.com'
 export const ConfigContext = createContext();
 
 export const ConfigProvider = ({ children }) => {
   const [config, setConfig] = useState({
-    apiUrl: localhost,
+    apiUrl: prodURL,
   });
 
   return (
